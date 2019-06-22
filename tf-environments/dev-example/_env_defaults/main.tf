43679f368f1fa01315505c5f096788ea01bcfd1f
garland <garlandk@gmail.com>
2019-06-22 12:16:12 -0700

    Adding dev-example vpc
    Signed-off-by: garland <garlandk@gmail.com>

---
+++ tf-environments/dev-example/_env_defaults/main.tf
@@ -0,0 +1,27 @@
+output environment_name {
+  value = "dev-example"
+}
+
+output aws_region {
+  value = "us-east-1"
+}
+
+output vpc_cidr {
+  value = "10.10.0.0/16"
+}
+
+output vpc_id {
+  value = "vpc-fill-me-in-after-your-vpc-has-been-created"
+}
+
+output aws_availability_zone_1 {
+  value = "a"
+}
+
+output aws_availability_zone_2 {
+  value = "b"
+}
+
+output aws_availability_zone_3 {
+  value = "c"
+}
