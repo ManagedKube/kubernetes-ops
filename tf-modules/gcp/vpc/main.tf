4ffea2590d23a79cda437bebaac6181e8c660080
garland <garlandk@gmail.com>
2019-07-09 13:18:25 -0700

    Routing nat from all IP ranges
    Signed-off-by: garland <garlandk@gmail.com>

--- tf-modules/gcp/vpc/main.tf
+++ tf-modules/gcp/vpc/main.tf
@@ -52,24 +52,21 @@ resource "google_compute_router" "router" {
 resource "google_compute_address" "address" {
   count  = 2
   name   = "${var.vpc_name}-nat-external-address-${count.index}"
-  region = "us-central1"
+  region = "${var.region}"
 }
 
 resource "google_compute_router_nat" "advanced-nat" {
   provider                           = "google-beta"
   name                               = "${var.vpc_name}-nat-1"
   router                             = "${google_compute_router.router.name}"
-  region                             = "us-central1"
-  nat_ip_allocate_option             = "MANUAL_ONLY"
-  nat_ips                            = ["${google_compute_address.address.*.self_link}"]
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-  subnetwork {
-    name                     = "${google_compute_subnetwork.private_subnet.self_link}"
-    source_ip_ranges_to_nat  = ["ALL_IP_RANGES"]
-  }
-  # log_config {
-  #   filter = "TRANSLATIONS_ONLY"
-  #   enable = true
+  region                             = "${var.region}"
+  nat_ip_allocate_option             = "AUTO_ONLY" #"MANUAL_ONLY"
+  # nat_ip_allocate_option             = "MANUAL_ONLY"
+  # nat_ips                            = ["${google_compute_address.address.*.self_link}"]
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+  # subnetwork {
+  #   name                    = "${google_compute_subnetwork.subnetwork.self_link}"
+  #   source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   # }
 }
 
