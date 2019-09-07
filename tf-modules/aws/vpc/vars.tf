972e49eda7ae68346e0aae24389c33bfa2902556
garland <garlandk@gmail.com>
2019-09-07 15:13:11 -0700

    Updating vpc module
    Signed-off-by: garland <garlandk@gmail.com>

--- tf-modules/aws/vpc/vars.tf
+++ tf-modules/aws/vpc/vars.tf
@@ -1,15 +1,15 @@
 # Required
 
 variable "tags" {
-  type = "map"
+  type = map(string)
 
   default = {
-    Name            = "dev",
-    Environment     = "env",
-    Account         = "dev",
-    Group           = "devops",
-    Region          = "us-east-1",
-    managed_by      = "Terraform"
+    Name        = "dev"
+    Environment = "env"
+    Account     = "dev"
+    Group       = "devops"
+    Region      = "us-east-1"
+    managed_by  = "Terraform"
   }
 }
 
@@ -23,24 +23,25 @@ variable "vpc_cidr" {
 
 variable "availability_zones" {
   description = "AZs for subnets i.e. [us-east-1a, us-east-1b]"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "public_cidrs" {
   description = "CIDR block for public subnets (should be the same amount as AZs)"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "private_cidrs" {
   description = "CIDR block for private subnets (should be the same amount as AZs)"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "optional_vpc_tags" {
   default = {}
-  type    = "map"
+  type    = map(string)
 }
 
 variable "security_group_default_egress" {
   default = ["0.0.0.0/0"]
 }
+
