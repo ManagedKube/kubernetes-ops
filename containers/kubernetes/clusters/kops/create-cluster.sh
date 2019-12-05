f5dcc37ee649ad46fe004333123c167ad1b48065
sekka1 <garlandk@gmail.com>
2019-12-05 15:24:42 -0800

    GitHub actions (#42)
    * Adding test workflow
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding test workflow
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding more
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding docs
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding examples on the fargate runner
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Kops updates
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding to docs
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding to docs
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding dev-test values file
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Downloading sonobuoy
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Downloading sonobuoy
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Downloading sonobuoy
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Downloading sonobuoy
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Downloading sonobuoy
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Downloading sonobuoy
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding rant on how to use github actions
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * update container
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Parameterizing the versions
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Updating cluster to 1.14.x
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Updating sonobuoy download
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * changing directory structure
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding create clusters
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Fixing cluster creation
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding flag to pull from the temp output folder for the cluster name
    
    Signed-off-by: gar <garlandk@gmail.com>

---
+++ containers/kubernetes/clusters/kops/create-cluster.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -ex
+
+CLUSTER_NAME=ci-pipeline
+
+TMP_OUTPUT_LOCATION=./tmp-output
+
+echo "Remove temporary output if it exist"
+rm -rf ${TMP_OUTPUT_LOCATION} || true
+
+echo "Create temporary output folder"
+mkdir ${TMP_OUTPUT_LOCATION}
+
+echo "Generate random UUID"
+NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
+
+CLUSTER_NAME=ci-pipeline-${NEW_UUID}
+
+echo "Copy ci-pipeline folder for this CI run's usage"
+cp -a ./clusters/aws/kops/clusters/ci-pipeline ./clusters/aws/kops/clusters/${CLUSTER_NAME}
+
+echo "Output cluster name into the temporary folder"
+echo "${CLUSTER_NAME}" > ${TMP_OUTPUT_LOCATION}/cluster-name.txt
+
+echo "Replace kops values file with the correct name parameters"
+sed -i "s/ci-pipeline/${CLUSTER_NAME}/g" ./clusters/aws/kops/clusters/${CLUSTER_NAME}/values.yaml
+
+cd ./clusters/aws/kops/
+
+echo "Creating a new kops cluster [DRY RUN]"
+./kops.sh --name ${CLUSTER_NAME} --create true --dry-run true
+
+echo "Creating a new kops cluster [NOT DRY RUN]"
+./kops.sh --name ${CLUSTER_NAME} --create true --dry-run false
+
+echo "cluster name: ${CLUSTER_NAME}"
---
+++ containers/kubernetes/clusters/kops/e2e-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+echo "Run sonobuoy Kubernetes e2e tests"
+echo "Running e2e tests"
+
+# Delete previous test
+sonobuoy delete --all --wait
+
+# Long test
+sonobuoy run --e2e-focus="\\[Conformance\\]" --e2e-skip="(\[Serial\])" --wait
+
+# Quick test
+# sonobuoy run --mode quick --wait
+
+# Get results
+results=$(sonobuoy retrieve)
+sonobuoy results $results
