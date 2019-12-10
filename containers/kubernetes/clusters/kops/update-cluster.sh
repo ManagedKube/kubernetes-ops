4d63d9dded35d79ec7b14d5990c4de45eee55e6b
gar <garlandk@gmail.com>
2019-12-10 15:50:07 -0800

    Changing cloud only to a variable
    Signed-off-by: gar <garlandk@gmail.com>

--- containers/kubernetes/clusters/kops/update-cluster.sh
+++ containers/kubernetes/clusters/kops/update-cluster.sh
@@ -4,6 +4,8 @@ if [ ! -z "${DEBUG}" ]; then
   set -x
 fi
 
+#CLOUD_ONLY="--cloudonly true"
+
 # Set the environment name to the tmp-output name if it exist
 if [ -a ./tmp-output/cluster-name.txt ]; then
     echo "hello"
@@ -21,7 +23,7 @@ echo "Apply kops update [NOT DRY RUN]"
 ./kops.sh --name ${ENVIRONMENT_NAME} --update true --dry-run false
 
 echo "Apply kops rolling update [DRY RUN]"
-./kops.sh --name ${ENVIRONMENT_NAME} --rolling-update true --cloudonly true --dry-run true
+./kops.sh --name ${ENVIRONMENT_NAME} --rolling-update true ${CLOUD_ONLY} --dry-run true
 
 echo "Apply kops rolling update [NOT DRY RUN]"
-./kops.sh --name ${ENVIRONMENT_NAME} --rolling-update true --cloudonly true --dry-run false
+./kops.sh --name ${ENVIRONMENT_NAME} --rolling-update true ${CLOUD_ONLY} --dry-run false
