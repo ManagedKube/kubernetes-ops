c4a2acf978def1e78f014bfa136dd6c99212ec9d
gar <garlandk@gmail.com>
2019-12-11 05:59:16 -0800

    Adding flag to e2e test for long/short
    Signed-off-by: gar <garlandk@gmail.com>

--- containers/kubernetes/clusters/kops/e2e-tests.sh
+++ containers/kubernetes/clusters/kops/e2e-tests.sh
@@ -10,11 +10,13 @@ echo "Running e2e tests"
 # Delete previous test
 sonobuoy delete --all --wait
 
-# Long test
-# sonobuoy run --e2e-focus="\\[Conformance\\]" --e2e-skip="(\[Serial\])" --wait
-
-# Quick test
-sonobuoy run --mode quick --wait
+if [ ! -z "${E2E_TESTS_QUICK_MODE}" ]; then
+  # Quick test
+  sonobuoy run --mode quick --wait
+else
+  # Long test
+  sonobuoy run --e2e-focus="\\[Conformance\\]" --e2e-skip="(\[Serial\])" --wait
+fi
 
 # Get results
 results=$(sonobuoy retrieve)
