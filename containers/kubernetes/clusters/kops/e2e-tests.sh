6541d6d45fb3eab1625c88466d6ee10328e54e4e
gar <garlandk@gmail.com>
2019-12-11 06:11:36 -0800

    fixing merge conflict
    Signed-off-by: gar <garlandk@gmail.com>

--- containers/kubernetes/clusters/kops/e2e-tests.sh
+++ containers/kubernetes/clusters/kops/e2e-tests.sh
@@ -10,13 +10,6 @@ echo "Running e2e tests"
 # Delete previous test
 sonobuoy delete --all --wait
 
-<<<<<<< HEAD
-# Long test
-# sonobuoy run --e2e-focus="\\[Conformance\\]" --e2e-skip="(\[Serial\])" --wait
-
-# Quick test
-sonobuoy run --mode quick --wait
-=======
 if [ ! -z "${E2E_TESTS_QUICK_MODE}" ]; then
   # Quick test
   sonobuoy run --mode quick --wait
@@ -24,7 +17,6 @@ else
   # Long test
   sonobuoy run --e2e-focus="\\[Conformance\\]" --e2e-skip="(\[Serial\])" --wait
 fi
->>>>>>> cfd18a2c68d4f4a33843a54cbb683c865a06f244
 
 # Get results
 results=$(sonobuoy retrieve)
