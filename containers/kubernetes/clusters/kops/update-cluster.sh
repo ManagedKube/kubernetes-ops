98ff1dceb5039f74fb2e6a8cb217ed604e3d20bb
sekka1 <garlandk@gmail.com>
2019-12-30 13:58:39 -0800

    Fixing bugs (#55)
    Signed-off-by: gar <garlandk@gmail.com>

--- containers/kubernetes/clusters/kops/update-cluster.sh
+++ containers/kubernetes/clusters/kops/update-cluster.sh
@@ -8,12 +8,11 @@ fi
 
 # Set the environment name to the tmp-output name if it exist
 if [ -a ./tmp-output/cluster-name.txt ]; then
-    echo "hello"
     ENVIRONMENT_NAME=$(cat ./tmp-output/cluster-name.txt)
-    echo ${ENVIRONMENT_NAME}
+    echo "ENVIRONMENT_NAME: ${ENVIRONMENT_NAME}"
 fi
 
-cd ./clusters/aws/kops
+cd ./clusters/aws/kops/
 
 echo "Apply kops update [DRY RUN]"
 ./kops.sh --name ${ENVIRONMENT_NAME} --update true --dry-run true
