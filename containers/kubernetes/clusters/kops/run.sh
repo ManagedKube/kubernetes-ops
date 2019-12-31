3f86de39c11f8a828e766d902e36ccb8099c5ec6
gar <garlandk@gmail.com>
2019-12-30 21:41:56 -0800

    adding run.sh script
    Signed-off-by: gar <garlandk@gmail.com>

---
+++ containers/kubernetes/clusters/kops/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -ex
+
+CLUSTER_NAME="fargate-cluster"
+PIPELINE_VERSION=20
+
+# Start Fargate Task
+TASK_ARN=$(aws ecs run-task --cluster ${CLUSTER_NAME}  --task-definition pipeline:${PIPELINE_VERSION} --launch-type "FARGATE" --network-configuration "awsvpcConfiguration={subnets=[subnet-0121a9057485fbe72],securityGroups=[sg-01b56214e8d158906]}" | jq -r .tasks[0].taskArn)
+
+# Poll until lastStatus is RUNNING
+IS_DONE=false
+until ${IS_DONE}
+do
+    echo "Fargate task is not in a running state yet...wait and poll again. | lastStatus: ${STATUS}"
+    sleep 2
+
+    STATUS=$(aws ecs describe-tasks --cluster ${CLUSTER_NAME} --tasks ${TASK_ARN} | jq -r .tasks[0].lastStatus)
+    if [ "${STATUS}" == "RUNNING" ]; then
+        IS_DONE=true
+    fi
+done    
+
+TASK_ID=$(echo ${TASK_ARN} | grep -o -e "\/.*" | grep -o -e "[0-9a-z].*")
+
+# Follow logs until it has completed
+ecs-cli logs --cluster ${CLUSTER_NAME} --task-id ${TASK_ID} --follow
