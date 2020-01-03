baf53188fe6739b37fe03a3ac883ffff54c26533
gar <garlandk@gmail.com>
2020-01-02 21:30:02 -0800

    Handling the case with one cluster
    Signed-off-by: gar <garlandk@gmail.com>

--- containers/kubernetes/clusters/kops/cleanup.sh
+++ containers/kubernetes/clusters/kops/cleanup.sh
@@ -15,14 +15,15 @@ fi
 CLUSTER_NAME_PREFIX="ci-pipeline"
 
 if [ "${DELETE_PREVIOUS_CLUSTER}" == "true" ]; then
-    
-    # Loop through the kops get clusters output and get each cluster name
-    for row in $(kops get clusters --output json | jq -r '.[] | @base64'); do
-        _jq() {
-            echo ${row} | base64 --decode | jq -r ${1}
-        }
 
-        CLUSTER_NAME=$(_jq '.metadata.name')
+    # Kops returns an array of clusters if there are more than one.
+    # If there is only one, it returns a json element with just one element
+    # We have to treat these two cases differently.
+    NUMBER_OF_CLUSTERS=$(kops get clusters | wc -l)
+    if [ "${NUMBER_OF_CLUSTERS}" -eq 2 ]; then
+        echo "There is one cluster."
+
+        CLUSTER_NAME=$(kops get clusters --output json | jq -r .metadata.name)
 
         # Delete all cluster staring with ${CLUSTER_NAME_PREFIX} in the name
         if echo ${CLUSTER_NAME} | grep -e "^${CLUSTER_NAME_PREFIX}"; then
@@ -30,6 +31,24 @@ if [ "${DELETE_PREVIOUS_CLUSTER}" == "true" ]; then
             echo "Deleting cluster: ${CLUSTER_NAME}"
             kops delete cluster ${CLUSTER_NAME} --yes
         fi
-    done
+    else
+
+        # Loop through the kops get clusters output and get each cluster name
+        for row in $(kops get clusters --output json | jq -r '.[] | @base64'); do
+            _jq() {
+                echo ${row} | base64 --decode | jq -r ${1}
+            }
+
+            CLUSTER_NAME=$(_jq '.metadata.name')
+
+            # Delete all cluster staring with ${CLUSTER_NAME_PREFIX} in the name
+            if echo ${CLUSTER_NAME} | grep -e "^${CLUSTER_NAME_PREFIX}"; then
+
+                echo "Deleting cluster: ${CLUSTER_NAME}"
+                kops delete cluster ${CLUSTER_NAME} --yes
+            fi
+        done
+
+    fi
 
 fi
