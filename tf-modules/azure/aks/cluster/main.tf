75f4c5e523db1cd1b1b0e77ca16589f3bcf218ab
Marcelo Grebois <mgrebois@dc.uba.ar>
2020-05-06 14:48:57 +0200

    Azure aks (#89)
    * Adding in initial Azure AKS cluster
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Updating pod policy
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    Co-authored-by: gar <garlandk@gmail.com>

---
+++ tf-modules/azure/aks/cluster/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+    version = "~>2.8.0"
+    features {}
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  kubernetes_version  = var.kubernetes_version
+  private_cluster_enabled = var.private_cluster_enabled
+  dns_prefix          = var.dns_prefix
+  api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
+  enable_pod_security_policy = var.enable_pod_security_policy
+
+  role_based_access_control {
+      enabled = var.role_based_access_control_enabled
+  }
+
+  network_profile {
+      network_plugin = var.network_profile_network_plugin
+      network_policy = var.network_profile_network_policy
+      pod_cidr       = var.network_profile_pod_cidr
+  }
+
+  default_node_pool {
+    name       = var.default_node_pool_name
+    node_count = var.default_node_pool_node_count
+    vm_size    = var.default_node_pool_instance_size
+    enable_auto_scaling = var.default_node_pool_enable_auto_scaling
+    max_count = var.default_node_pool_max_count
+    min_count = var.default_node_pool_min_count
+    os_disk_size_gb = var.default_node_pool_os_disk_size_gb
+
+    node_labels = var.default_node_pool_node_labels
+    node_taints = var.default_node_pool_node_taints
+  }
+
+  addon_profile {
+    kube_dashboard {
+      enabled = var.kube_dashboard_enabled
+    }
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags   = var.tags
+}
