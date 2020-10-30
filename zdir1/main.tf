fdae91db3c7ca1a8b64e74ca7408379187181356
gar <garlandk@gmail.com>
2020-10-30 16:41:01 -0700

    Updating instance size
    Signed-off-by: gar <garlandk@gmail.com>

--- zdir1/main.tf
+++ zdir1/main.tf
@@ -27,7 +27,7 @@ resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
-  instance_type          = "t2.micro"
+  instance_type          = "t2.large"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   user_data = <<-EOF
