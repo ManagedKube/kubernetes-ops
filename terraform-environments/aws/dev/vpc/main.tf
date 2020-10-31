119f2ab3db103cc0a0f55f810ded1339b4a73d69
gar <garlandk@gmail.com>
2020-10-30 19:58:44 -0700

    Adding vpc
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-environments/aws/dev/vpc/main.tf
+++ terraform-environments/aws/dev/vpc/main.tf
@@ -21,36 +21,6 @@ provider "aws" {
   region = "us-west-2"
 }
 
-provider "random" {}
-
-resource "random_pet" "sg" {}
-
-resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
-  instance_type          = "t2.large"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
-}
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
