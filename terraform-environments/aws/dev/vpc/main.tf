0dac2be82b642be3d0139e7a244b6ef2da77608b
sekka1 <garlandk@gmail.com>
2020-10-30 21:04:03 -0700

    Addig vpc output (#100)
    * Addig vpc output
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Addig region output
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding more outputs
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding more outputs
    
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-environments/aws/dev/vpc/main.tf
+++ terraform-environments/aws/dev/vpc/main.tf
@@ -40,3 +40,16 @@ module "vpc" {
     Foo         = "bar"
   }
 }
+
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
