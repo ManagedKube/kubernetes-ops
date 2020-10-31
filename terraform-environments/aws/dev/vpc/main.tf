47e4c23b2b8209f6a5b0fe0cd1f02d08459d2d41
gar <garlandk@gmail.com>
2020-10-30 20:02:30 -0700

    Updating region
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-environments/aws/dev/vpc/main.tf
+++ terraform-environments/aws/dev/vpc/main.tf
@@ -27,7 +27,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
