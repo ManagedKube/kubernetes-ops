c90f66f504c8667ab68b909213864dbaa5809057
gar <garlandk@gmail.com>
2020-10-30 19:52:14 -0700

    Testing tf
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-environments/aws/dev/vpc/main.tf
+++ terraform-environments/aws/dev/vpc/main.tf
@@ -21,22 +21,32 @@ provider "aws" {
   region = "us-west-2"
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "2.63.0"
+provider "random" {}
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+resource "random_pet" "sg" {}
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+resource "aws_instance" "web" {
+  ami                    = "ami-830c94e3"
+  instance_type          = "t2.large"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+}
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
 }
