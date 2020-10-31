849c91da12039d23ef3de6e6fc05d4be63196fd3
sekka1 <garlandk@gmail.com>
2020-10-30 17:06:41 -0700

    Tf gh action (#94)
    * Adding testing tf files
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Commenting out all of the kops workflow
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Fixing backend
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding working-dir
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding tf file
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding path inclusion
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding path inclusion
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Renaming gh action file
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Updating instance size
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * Adding vpc tf
    
    Signed-off-by: gar <garlandk@gmail.com>

---
+++ terraform-environments/aws/dev/vpc/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {
+    organization = "managedkube"
+
+    workspaces {
+      name = "kubernetes-ops"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "2.63.0"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
