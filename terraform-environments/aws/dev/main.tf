a93ab6432a88398b5aef84c7d8489e494fc9662e
gar <garlandk@gmail.com>
2021-04-22 23:51:58 +0000

    Adding kube auth info
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-environments/aws/dev/main.tf
+++ terraform-environments/aws/dev/main.tf
@@ -26,6 +26,17 @@ provider "aws" {
 #
 # EKS authentication
 #
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", var.environment_name]
+      command     = "aws"
+    }
+  }
+}
 # data "aws_eks_cluster_auth" "eks" {
 #   name = module.eks.cluster_id
 # }
--- terraform-modules/aws/eks/outputs.tf
+++ terraform-modules/aws/eks/outputs.tf
@@ -14,3 +14,7 @@ output "cluster_certificate_authority_data" {
 output "cluster_id" {
   value = module.eks.cluster_id
 }
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
