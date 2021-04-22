f7adf8bfb48294424c08be9101d66cb17ce73372
gar <garlandk@gmail.com>
2021-04-22 23:52:50 +0000

    Adding kube auth info
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-modules/aws/eks/outputs.tf
+++ terraform-modules/aws/eks/outputs.tf
@@ -14,7 +14,3 @@ output "cluster_certificate_authority_data" {
 output "cluster_id" {
   value = module.eks.cluster_id
 }
-
-output "cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
-}
