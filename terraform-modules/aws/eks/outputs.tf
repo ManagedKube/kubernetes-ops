390d65e0179375bf3d122aab5bd56e9e065475d3
gar <garlandk@gmail.com>
2021-04-22 23:46:39 +0000

    Adding output
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-modules/aws/eks/outputs.tf
+++ terraform-modules/aws/eks/outputs.tf
@@ -12,9 +12,5 @@ output "cluster_certificate_authority_data" {
 }
 
 output "cluster_id" {
-  description = "The name/id of the EKS cluster. Will block on cluster creation until the cluster is really ready"
-  value       = element(concat(data.aws_eks_cluster.*.id, list("")), 0)
-  # So that calling plans wait for the cluster to be available before attempting
-  # to use it. They will not need to duplicate this null_resource
-  depends_on = [null_resource.wait_for_cluster]
+  value = module.eks.cluster_id
 }
