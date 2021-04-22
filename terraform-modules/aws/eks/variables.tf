78634afd2a0d84ed636f6ecc379b5c728123ec79
gar <garlandk@gmail.com>
2021-04-22 22:34:43 +0000

    Ran terraform fmt
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-modules/aws/eks/variables.tf
+++ terraform-modules/aws/eks/variables.tf
@@ -1,50 +1,50 @@
-variable aws_region {
-    default = "us-east-1"
+variable "aws_region" {
+  default = "us-east-1"
 }
-variable tags {}
+variable "tags" {}
 variable "vpc_id" {
-    default = ""
+  default = ""
 }
-variable private_subnets {
-    type = list
-    default = []
+variable "private_subnets" {
+  type    = list(any)
+  default = []
 }
-variable public_subnets {
-    type = list
-    default = []
-}
-
-variable cluster_name {
-    default = "test-cluster"
+variable "public_subnets" {
+  type    = list(any)
+  default = []
 }
 
-variable cluster_version {
-    default = "1.18"
+variable "cluster_name" {
+  default = "test-cluster"
 }
 
-variable enable_irsa {
+variable "cluster_version" {
+  default = "1.18"
+}
+
+variable "enable_irsa" {
   type        = bool
   default     = true
   description = "enable_irsa"
 }
 
-variable cluster_endpoint_public_access {
+variable "cluster_endpoint_public_access" {
   type        = bool
   default     = true
   description = "Enable or disable Kube API public access"
 }
 
-variable cluster_endpoint_public_access_cidrs {
-  type        = list
-  default     = [
+variable "cluster_endpoint_public_access_cidrs" {
+  type = list(any)
+  default = [
     "0.0.0.0/0"
   ]
   description = "Kube API public endpoint allow access cidrs"
 }
 
-variable map_roles {
-  type        = list
-  default     = [
+variable "map_roles" {
+  type = list(any)
+  default = [
     {
       rolearn  = "arn:aws:iam::66666666666:role/role1"
       username = "role1"
@@ -54,9 +54,9 @@ variable map_roles {
   description = "A list of roles to give permission to access this cluster"
 }
 
-variable map_users {
-  type        = list
-  default     = [
+variable "map_users" {
+  type = list(any)
+  default = [
     {
       userarn  = "arn:aws:iam::66666666666:user/user1"
       username = "user1"
@@ -71,27 +71,27 @@ variable map_users {
   description = "A list of users to give permission to access this cluster"
 }
 
-variable node_groups {
-  type        = any
-  default     = {
+variable "node_groups" {
+  type = any
+  default = {
     ng1 = {
       disk_size        = 20
       desired_capacity = 1
       max_capacity     = 1
       min_capacity     = 1
       instance_type    = "t2.small"
-      additional_tags  = {
+      additional_tags = {
         Name = "foo",
       }
-      k8s_labels       = {}
+      k8s_labels = {}
     }
   }
   description = "node group(s) configurations"
 }
 
-variable cluster_enabled_log_types {
-  type        = list(string)
-  default     = [
+variable "cluster_enabled_log_types" {
+  type = list(string)
+  default = [
     "api",
     "audit",
     "authenticator",
@@ -101,7 +101,7 @@ variable cluster_enabled_log_types {
   description = "The Kubernetes log types to enable"
 }
 
-variable cluster_log_retention_in_days {
+variable "cluster_log_retention_in_days" {
   type        = number
   default     = 90
   description = "Log retention in days"
