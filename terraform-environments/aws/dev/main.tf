1fbde77f0aad0d0100ee635921ad50ee64697077
gar <garlandk@gmail.com>
2021-04-23 02:48:19 +0000

    Updating prom values to add a disk
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-environments/aws/dev/main.tf
+++ terraform-environments/aws/dev/main.tf
@@ -165,7 +165,7 @@ module "nginx-ingress" {
 #
 #
 module "kube-prometheus-stack" {
-  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/helm/kube-prometheus-stack?ref=helm"
+  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/helm/kube-prometheus-stack?ref=v1.0.7"
 
   helm_values = file("${path.module}/helm_values/kube-prometheus-stack/values.yaml")
 
