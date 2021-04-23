8877ffb0f2224284fdad77bf305cc162e5f832a2
Garland Kan <garland@managedkube.com>
2021-04-22 19:14:16 -0700

    Adding subnet tags for the kube elbs (#117)
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-modules/aws/vpc/main.tf
+++ terraform-modules/aws/vpc/main.tf
@@ -16,5 +16,15 @@ module "vpc" {
   enable_nat_gateway = var.enable_nat_gateway
   enable_vpn_gateway = var.enable_vpn_gateway
 
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+
   tags = var.tags
 }
