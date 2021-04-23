20b4c95de19fcd78a6c83dbef18388c7c51d0949
Garland Kan <garland@managedkube.com>
2021-04-23 13:16:39 -0700

    Updating the var name (#129)
    * Updating the var name
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding depends on
    
    Signed-off-by: gar <garlandk@gmail.com>
    
    * adding depends on
    
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-environments/aws/dev/main.tf
+++ terraform-environments/aws/dev/main.tf
@@ -39,26 +39,34 @@ provider "aws" {
 #   }
 # }
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/eks_cluster_auth
-data "aws_eks_cluster" "example" {
+data "aws_eks_cluster" "eks_data" {
   name = var.environment_name
+
+  depends_on = [
+    module.eks
+  ]
 }
 
-data "aws_eks_cluster_auth" "example" {
+data "aws_eks_cluster_auth" "eks_data" {
   name = var.environment_name
+
+  depends_on = [
+    module.eks
+  ]
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.example.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.example.token
+  host                   = data.aws_eks_cluster.eks_data.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_data.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks_data.token
   load_config_file       = false
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.example.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.example.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.example.token
+    host                   = data.aws_eks_cluster.eks_data.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_data.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks_data.token
   }
 }
 
