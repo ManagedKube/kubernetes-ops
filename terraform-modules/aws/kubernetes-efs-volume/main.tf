c0872fa8930bb4877a0bd8929d90ee14f9a31e61
gar <garlandk@gmail.com>
2021-05-08 20:41:05 +0000

    Adding back pv and sc
    Signed-off-by: gar <garlandk@gmail.com>

--- terraform-modules/aws/kubernetes-efs-volume/main.tf
+++ terraform-modules/aws/kubernetes-efs-volume/main.tf
@@ -27,45 +27,45 @@ module "efs" {
   tags = var.tags
 }
 
-# resource "kubernetes_storage_class" "storage_class" {
-#   metadata {
-#     name = "${var.efs_name}-sc"
-#   }
-#   storage_provisioner = "efs.csi.aws.com"
-#   reclaim_policy      = "Retain"
-#   # https://github.com/kubernetes-sigs/aws-efs-csi-driver/tree/master/examples/kubernetes/dynamic_provisioning#dynamic-provisioning
-#   parameters = {
-#     provisioningMode = "efs-ap"
-#     fileSystemId     = module.efs.outputs.id
-#     directoryPerms   = "700"
-#     gidRangeStart    = "1000"
-#     gidRangeEnd      = "2000"
-#     basePath         = "/"
-#   }
-#   # mount_options = ["file_mode=0700", "dir_mode=0777", "mfsymlinks", "uid=1000", "gid=1000", "nobrl", "cache=none"]
-#   mount_options = ["tls"]
+resource "kubernetes_storage_class" "storage_class" {
+  metadata {
+    name = "${var.efs_name}-sc"
+  }
+  storage_provisioner = "efs.csi.aws.com"
+  reclaim_policy      = "Retain"
+  # https://github.com/kubernetes-sigs/aws-efs-csi-driver/tree/master/examples/kubernetes/dynamic_provisioning#dynamic-provisioning
+  parameters = {
+    provisioningMode = "efs-ap"
+    fileSystemId     = module.efs.outputs.id
+    directoryPerms   = "700"
+    gidRangeStart    = "1000"
+    gidRangeEnd      = "2000"
+    basePath         = "/"
+  }
+  # mount_options = ["file_mode=0700", "dir_mode=0777", "mfsymlinks", "uid=1000", "gid=1000", "nobrl", "cache=none"]
+  mount_options = ["tls"]
 
-#   depends_on = [
-#     module.efs
-#   ]
-# }
+  depends_on = [
+    module.efs
+  ]
+}
 
-# resource "kubernetes_persistent_volume" "pv" {
-#   metadata {
-#     name = var.efs_name
-#   }
-#   spec {
-#     storage_class_name               = "${var.efs_name}-sc"
-#     persistent_volume_reclaim_policy = "Retain"
-#     capacity = {
-#       storage = "2Gi"
-#     }
-#     access_modes = ["ReadWriteMany"]
-#     persistent_volume_source {
-#       nfs {
-#         path   = "/"
-#         server = module.efs.outputs.id
-#       }
-#     }
-#   }
-# }
+resource "kubernetes_persistent_volume" "pv" {
+  metadata {
+    name = var.efs_name
+  }
+  spec {
+    storage_class_name               = "${var.efs_name}-sc"
+    persistent_volume_reclaim_policy = "Retain"
+    capacity = {
+      storage = "2Gi"
+    }
+    access_modes = ["ReadWriteMany"]
+    persistent_volume_source {
+      nfs {
+        path   = "/"
+        server = module.efs.outputs.id
+      }
+    }
+  }
+}
