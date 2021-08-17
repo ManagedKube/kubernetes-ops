964b70d1667c145ff4251b00904466d05f510ff7
gar <garlandk@gmail.com>
2021-08-16 18:51:43 -0700

    Adding nginx-ingress

---
+++ terraform-environments/aws/dev/helm/40-ingress-nginx-external/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "environment_name" {
+  default = "dev"
+}
+
+variable "tags" {
+  type = map(any)
+  default = {
+    ops_env              = "dev"
+    ops_managed_by       = "terraform",
+    ops_source_repo      = "kubernetes-ops",
+    ops_source_repo_path = "terraform-environments/aws/dev/helm/40-ingress-nginx-external",
+    ops_owners           = "devops",
+  }
+}
