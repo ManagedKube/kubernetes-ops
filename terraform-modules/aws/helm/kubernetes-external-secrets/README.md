fafba84906012ab09b49648e0212f131941996f4
gar <garlandk@gmail.com>
2021-09-21 08:44:22 -0700

    kubernetes-external-secrets

---
+++ terraform-modules/aws/helm/kubernetes-external-secrets/README.md
@@ -0,0 +1,5 @@
+#  kubernetes-external-secrets
+
+https://github.com/external-secrets/kubernetes-external-secrets#system-architecture
+
+
---
+++ terraform-modules/aws/helm/kubernetes-external-secrets/variables.tf
@@ -0,0 +1,35 @@
+variable helm_version {
+  type        = string
+  default     = "8.3.0"
+  description = "Helm chart version"
+}
+
+variable verify {
+  type        = bool
+  default     = false
+  description = "Verify the helm download"
+}
+
+variable "create_namespace" {
+  type = bool
+  default = true
+  description = "Create namespace if it does not exist"
+}
+
+variable namespace {
+  type        = string
+  default     = "kubernetes-external-secrets"
+  description = "Namespace to install in"
+}
+
+variable chart_name {
+  type        = string
+  default     = "kubernetes-external-secrets"
+  description = "Name to set the helm deployment to"
+}
+
+variable helm_values {
+  type        = string
+  default     = ""
+  description = "Additional helm values to pass in.  These values would override the default in this module."
+}
