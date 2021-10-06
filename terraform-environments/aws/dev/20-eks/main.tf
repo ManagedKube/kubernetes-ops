57bd98fd9f7f8bb29db2710ec75a61a4e626c731
Garland Kan <garland@managedkube.com>
2021-10-06 16:18:35 -0700

    Creating EKS cluster (#188)

---
+++ terraform-environments/aws/dev/20-eks/main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.37.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {
+    organization = "managedkube"
+
+    workspaces {
+      name = "kubernetes-ops-staging-20-eks"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+  config = {
+    organization = "managedkube"
+    workspaces = {
+      name = "kubernetes-ops-staging-10-vpc"
+    }
+  }
+}
+
+#
+# EKS
+#
+module "eks" {
+  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/eks?ref=v1.0.22"
+
+  aws_region = var.aws_region
+  tags       = var.tags
+
+  cluster_name = var.environment_name
+
+  vpc_id         = data.terraform_remote_state.vpc.outputs.vpc_id
+  k8s_subnets    = data.terraform_remote_state.vpc.outputs.k8s_subnets
+  public_subnets = data.terraform_remote_state.vpc.outputs.public_subnets
+
+  cluster_version                = "1.18"
+  cluster_endpoint_public_access = true
+  cluster_endpoint_public_access_cidrs = [
+    "0.0.0.0/0"
+  ]
+
+  map_roles = [
+    {
+      rolearn  = "arn:aws:iam::66666666666:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
+  ]
+  map_users = [
+    {
+      userarn  = "arn:aws:iam::725654443526:user/username"
+      username = "username"
+      groups   = ["system:masters"]
+    },
+  ]
+
+  node_groups = {
+    ng1 = {
+      disk_size        = 20
+      desired_capacity = 2
+      max_capacity     = 4
+      min_capacity     = 1
+      instance_types   = ["t3.small"]
+      additional_tags  = var.tags
+      k8s_labels       = {}
+    }
+  }
+
+  # depends_on = [
+  #   module.vpc
+  # ]
+}
