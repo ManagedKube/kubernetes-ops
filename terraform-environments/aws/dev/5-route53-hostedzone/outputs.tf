b3e0f19c7cc453bf138a0517f7c147e14fbb934b
gar <garlandk@gmail.com>
2021-10-06 10:56:58 -0700

    Removing dev

--- terraform-environments/aws/dev/5-route53-hostedzone/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "zone_id" {
-  description = "The hosted zone ID"
-  value       = module.route53-hostedzone.zone_id
-}
-
-output "name_servers" {
-  description = "The hosted zone name servers"
-  value       = module.route53-hostedzone.name_servers
-}
