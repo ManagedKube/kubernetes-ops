a59df48042735b21bdd10d4e9a1c4f550fcd47f7
Garland Kan <garland@managedkube.com>
2021-10-06 14:56:30 -0700

    Updating the vpc module (#186)

--- terraform-environments/aws/dev/10-vpc/main.tf
+++ terraform-environments/aws/dev/10-vpc/main.tf
@@ -26,7 +26,7 @@ provider "aws" {
 # VPC
 #
 module "vpc" {
-  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/vpc?ref=v1.0.18"
+  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/vpc?ref=vpc"
 
   aws_region       = var.aws_region
   azs              = ["us-east-1a", "us-east-1c", "us-east-1d"]
