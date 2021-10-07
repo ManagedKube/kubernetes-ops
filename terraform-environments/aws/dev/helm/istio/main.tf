0eb9592dfb47cf33901301eba40d36a3259b386c
gar <garlandk@gmail.com>
2021-10-07 05:54:29 -0700

    Removing local source comment

--- terraform-environments/aws/dev/helm/istio/main.tf
+++ terraform-environments/aws/dev/helm/istio/main.tf
@@ -77,7 +77,6 @@ provider "helm" {
 #
 module "istio" {
   source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/istio?ref=main"
-  # source = "../../../../../terraform-modules/aws/istio"
 
   helm_values_istio_base = file("${path.module}/istio_base_values.yaml")
   helm_values_istiod     = file("${path.module}/istiod_values.yaml")
