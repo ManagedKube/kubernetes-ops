a7b61762b58ad1583d5580a5fa6c12717246409f
gar <garlandk@gmail.com>
2021-10-06 17:09:34 -0700

    Adding eks

--- terraform-environments/aws/dev/20-eks/main.tf
+++ terraform-environments/aws/dev/20-eks/main.tf
@@ -1,3 +1,15 @@
+locals {
+  aws_region       = "us-east-1"
+  environment_name = "dev"
+  tags = {
+    ops_env              = "${local.environment_name}"
+    ops_managed_by       = "terraform",
+    ops_source_repo      = "kubernetes-ops",
+    ops_source_repo_path = "terraform-environments/aws/${local.environment_name}/20-eks",
+    ops_owners           = "devops",
+  }
+}
+
 terraform {
   required_providers {
     aws = {
@@ -13,13 +25,13 @@ terraform {
     organization = "managedkube"
 
     workspaces {
-      name = "kubernetes-ops-staging-20-eks"
+      name = "kubernetes-ops-dev-20-eks"
     }
   }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = local.aws_region
 }
 
 data "terraform_remote_state" "vpc" {
@@ -27,7 +39,7 @@ data "terraform_remote_state" "vpc" {
   config = {
     organization = "managedkube"
     workspaces = {
-      name = "kubernetes-ops-staging-10-vpc"
+      name = "kubernetes-ops-${local.environment_name}-10-vpc"
     }
   }
 }
@@ -38,19 +50,30 @@ data "terraform_remote_state" "vpc" {
 module "eks" {
   source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/eks?ref=v1.0.22"
 
-  aws_region = var.aws_region
-  tags       = var.tags
+  aws_region = local.aws_region
+  tags       = local.tags
 
-  cluster_name = var.environment_name
+  cluster_name = local.environment_name
 
   vpc_id         = data.terraform_remote_state.vpc.outputs.vpc_id
   k8s_subnets    = data.terraform_remote_state.vpc.outputs.k8s_subnets
   public_subnets = data.terraform_remote_state.vpc.outputs.public_subnets
 
-  cluster_version                = "1.18"
+  cluster_version = "1.20"
+
+  # public cluster - kubernetes API is publicly accessible
   cluster_endpoint_public_access = true
   cluster_endpoint_public_access_cidrs = [
     "0.0.0.0/0"
+  ]
+
+  # private cluster - kubernetes API is internal the the VPC
+  cluster_endpoint_private_access                = true
+  cluster_create_endpoint_private_access_sg_rule = true
+  cluster_endpoint_private_access_cidrs = [
+    "10.0.0.0/8",
+    "172.16.0.0/12",
+    "192.168.0.0/16",
   ]
 
   map_roles = [
@@ -75,7 +98,7 @@ module "eks" {
       max_capacity     = 4
       min_capacity     = 1
       instance_types   = ["t3.small"]
-      additional_tags  = var.tags
+      additional_tags  = local.tags
       k8s_labels       = {}
     }
   }
