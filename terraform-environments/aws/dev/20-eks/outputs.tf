17fc34ac9afc9bdee58b004cc0b405563fb41cdb
gar <garlandk@gmail.com>
2021-10-06 19:49:22 -0700

    Creating EKS cluster

---
+++ terraform-environments/aws/dev/20-eks/outputs.tf
@@ -0,0 +1,15 @@
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+output "cluster_id" {
+  value = module.eks.cluster_id
+}
+
+output "cluster_oidc_issuer_url" {
+  value = module.eks.cluster_oidc_issuer_url
+}
