1248493a056f115f714a3f70446799c4b01fefdb
Garland Kan <garland@managedkube.com>
2021-10-06 20:58:02 -0700

    Cluster AutoScaler (#192)

---
+++ terraform-environments/aws/dev/25-eks-cluster-autoscaler/main.tf
@@ -0,0 +1,76 @@
+locals {
+  aws_region       = "us-east-1"
+  environment_name = "dev"
+  tags = {
+    ops_env              = "${local.environment_name}"
+    ops_managed_by       = "terraform",
+    ops_source_repo      = "kubernetes-ops",
+    ops_source_repo_path = "terraform-environments/aws/${local.environment_name}/25-eks-cluster-autoscaler",
+    ops_owners           = "devops",
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.37.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {
+    organization = "managedkube"
+
+    workspaces {
+      name = "kubernetes-ops-dev-25-eks-cluster-autoscaler"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+data "terraform_remote_state" "eks" {
+  backend = "remote"
+  config = {
+    organization = "managedkube"
+    workspaces = {
+      name = "kubernetes-ops-dev-20-eks"
+    }
+  }
+}
+
+#
+# EKS authentication
+# # https://registry.terraform.io/providers/hashicorp/helm/latest/docs#exec-plugins
+provider "helm" {
+  kubernetes {
+    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", "${local.environment_name}"]
+      command     = "aws"
+    }
+  }
+}
+
+#
+# Helm - cluster-autoscaler
+#
+module "cluster-autoscaler" {
+  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/cluster-autoscaler?ref=v1.0.12"
+
+  aws_region                  = local.aws_region
+  cluster_name                = local.environment_name
+  eks_cluster_id              = data.terraform_remote_state.eks.outputs.cluster_id
+  eks_cluster_oidc_issuer_url = data.terraform_remote_state.eks.outputs.cluster_oidc_issuer_url
+
+  depends_on = [
+    data.terraform_remote_state.eks
+  ]
+}
