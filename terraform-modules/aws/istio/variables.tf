4a0d70eabafc093c30c10afbe6108504cf4eea13
gar <garlandk@gmail.com>
2021-10-07 05:44:47 -0700

    Rounding out istio install

--- terraform-modules/aws/istio/variables.tf
+++ terraform-modules/aws/istio/variables.tf
@@ -4,6 +4,12 @@ variable "tags" {
   description = "Tags"
 }
 
+variable "verify" {
+  type        = bool
+  default     = false
+  description = "Verify the helm download"
+}
+
 variable "create_namespace" {
   type        = number
   default     = 1
@@ -38,6 +44,11 @@ variable "istio_version" {
   description = "The version of istio to install"
 }
 
+variable "install_helm_chart_istio_base" {
+  type        = number
+  default     = 1
+  description = "Install this helm chart or not"
+}
 variable "istio_base_chart_name" {
   type        = string
   default     = "istio-base"
@@ -45,6 +56,59 @@ variable "istio_base_chart_name" {
 }
 
 variable "helm_values_istio_base" {
+  type        = string
+  default     = ""
+  description = "Additional helm values to pass in.  These values would override the default in this module."
+}
+
+variable "install_helm_chart_istio_discovery" {
+  type        = number
+  default     = 1
+  description = "Install this helm chart or not"
+}
+variable "istio_discovery_chart_name" {
+  type        = string
+  default     = "istiod"
+  description = "The chart name for the istio-discovery helm install"
+}
+
+variable "helm_values_istiod" {
+  type        = string
+  default     = ""
+  description = "Additional helm values to pass in.  These values would override the default in this module."
+}
+
+variable "install_helm_chart_istio_ingress" {
+  type        = number
+  default     = 1
+  description = "Install this helm chart or not"
+}
+
+variable "istio_ingress_chart_name" {
+  type        = string
+  default     = "istio-ingress"
+  description = "The chart name for the istio-discovery helm install"
+}
+
+variable "helm_values_istio_ingress" {
+  type        = string
+  default     = ""
+  description = "Additional helm values to pass in.  These values would override the default in this module."
+}
+
+variable "install_helm_chart_istio_egress" {
+  type        = number
+  default     = 1
+  description = "Install this helm chart or not"
+}
+
+variable "istio_egress_chart_name" {
+  type        = string
+  default     = "istio-egress"
+  description = "The chart name for the istio-discovery helm install"
+}
+
+variable "helm_values_istio_egress" {
   type        = string
   default     = ""
   description = "Additional helm values to pass in.  These values would override the default in this module."
