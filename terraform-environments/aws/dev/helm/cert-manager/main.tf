5d4f2a7906149758d66300641f20d5f767d65283
gar <garlandk@gmail.com>
2021-10-07 20:02:34 -0700

    Adding cert-manager

---
+++ terraform-environments/aws/dev/helm/cert-manager/main.tf
@@ -0,0 +1,106 @@
+locals {
+  aws_region         = "us-east-1"
+  environment_name   = "dev"
+  lets_encrypt_email = "gar@managedkube.com"
+  tags = {
+    ops_env              = "${local.environment_name}"
+    ops_managed_by       = "terraform",
+    ops_source_repo      = "kubernetes-ops",
+    ops_source_repo_path = "terraform-environments/aws/${local.environment_name}/helm/cert-manager",
+    ops_owners           = "devops",
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.37.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "managedkube"
+
+    workspaces {
+      name = "kubernetes-ops-dev-helm-cert-manager"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.aws_region
+}
+
+data "terraform_remote_state" "eks" {
+  backend = "remote"
+  config = {
+    organization = "managedkube"
+    workspaces = {
+      name = "kubernetes-ops-dev-20-eks"
+    }
+  }
+}
+
+data "terraform_remote_state" "route53_hosted_zone" {
+  backend = "remote"
+  config = {
+    organization = "managedkube"
+    workspaces = {
+      name = "kubernetes-ops-dev-5-route53-hostedzone"
+    }
+  }
+}
+
+#
+# EKS authentication
+# # https://registry.terraform.io/providers/hashicorp/helm/latest/docs#exec-plugins
+provider "helm" {
+  kubernetes {
+    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", "${local.environment_name}"]
+      command     = "aws"
+    }
+  }
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = local.environment_name
+}
+
+provider "kubectl" {
+  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.main.token
+  load_config_file       = false
+}
+
+#
+# Helm - cluster-autoscaler
+#
+module "cert-manager" {
+  source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/helm/cert-manager?ref=v1.0.29"
+
+  aws_region                  = local.aws_region
+  cluster_name                = local.environment_name
+  eks_cluster_id              = data.terraform_remote_state.eks.outputs.cluster_id
+  eks_cluster_oidc_issuer_url = data.terraform_remote_state.eks.outputs.cluster_oidc_issuer_url
+  route53_hosted_zones        = data.terraform_remote_state.route53_hosted_zone.outputs.zone_id
+  domain_name                 = data.terraform_remote_state.route53_hosted_zone.outputs.domain_name
+  lets_encrypt_email          = local.lets_encrypt_email
+  helm_values_2               = file("${path.module}/values.yaml")
+
+  depends_on = [
+    data.terraform_remote_state.eks
+  ]
+}
