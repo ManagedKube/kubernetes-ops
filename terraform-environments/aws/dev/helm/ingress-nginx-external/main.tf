c1ce244570bae3bee2189909b177c3d69b7b5cd7
gar <garlandk@gmail.com>
2021-10-14 13:30:00 -0700

    Adding nginx default cert

--- terraform-environments/aws/dev/helm/ingress-nginx-external/main.tf
+++ terraform-environments/aws/dev/helm/ingress-nginx-external/main.tf
@@ -1,6 +1,7 @@
 locals {
   aws_region       = "us-east-1"
   environment_name = "dev"
+  namespace        = "ingress-nginx"
   tags = {
     ops_env              = "${local.environment_name}"
     ops_managed_by       = "terraform",
@@ -18,6 +19,10 @@ terraform {
     }
     random = {
       source = "hashicorp/random"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
     }
   }
 
@@ -44,6 +49,16 @@ data "terraform_remote_state" "eks" {
   }
 }
 
+data "terraform_remote_state" "route53_hosted_zone" {
+  backend = "remote"
+  config = {
+    organization = "gem-engineering"
+    workspaces = {
+      name = "kubernetes-ops-stage-5-route53-hostedzone"
+    }
+  }
+}
+
 provider "helm" {
   kubernetes {
     host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
@@ -54,6 +69,17 @@ provider "helm" {
       command     = "aws"
     }
   }
+}
+
+data "aws_eks_cluster_auth" "main" {
+  name = local.environment_name
+}
+
+provider "kubectl" {
+  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.main.token
+  load_config_file       = false
 }
 
 # Helm values file templating
@@ -76,11 +102,25 @@ module "ingress-nginx-external" {
   # The helm chart version you want to use
   helm_version = "3.30.0"
   # The namespace you want to install the chart into - it will create the namespace if it doesnt exist
-  namespace = "ingress-nginx"
+  namespace = local.namespace
   # The helm chart values file
   helm_values = data.template_file.helm_values.rendered
 
   depends_on = [
     data.terraform_remote_state.eks
   ]
+}
+
+# file templating
+data "template_file" "certificate" {
+  template = file("${path.module}/certificate.tpl.yaml")
+
+  vars = {
+    baseDomainName = data.terraform_remote_state.route53_hosted_zone.outputs.domain_name
+    namespace      = local.namespace
+  }
+}
+
+resource "kubectl_manifest" "certificate" {
+  yaml_body = data.template_file.certificate.rendered
 }
