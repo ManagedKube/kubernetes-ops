d76ca12a8057927a6f501b3fe43359b9b0784888
grebois <grebois@gmail.com>
2021-11-09 13:17:51 +0100

    Adding new loki-stack storing the logs on an s3 bucket

---
+++ terraform-modules/aws/loki-stack/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  type        = string
+  default     = "us-east-1"
+  description = "AWS region"
+}
+
+variable "cluster_name" {
+  type        = string
+  default     = "cluster"
+  description = "EKS cluster name"
+}
+
+variable "eks_cluster_oidc_issuer_url" {
+  type        = string
+  default     = ""
+  description = "EKS cluster oidc issuer url"
+}
