7af4385b27753476cfad975bf30c2368d2e67b28
gar <garlandk@gmail.com>
2021-11-22 17:13:10 -0800

    Updating security group inputs

--- terraform-modules/aws/mongodb-atlas/main.tf
+++ terraform-modules/aws/mongodb-atlas/main.tf
@@ -65,12 +65,12 @@ resource "aws_security_group" "this" {
   dynamic "egress" {
     for_each  =  var.egress_rule
     content {
-      description      = ingress.value["description"]
-      from_port        = ingress.value["from_port"]
-      to_port          = ingress.value["to_port"]
-      protocol         = ingress.value["protocol"]
-      cidr_blocks      = ingress.value["cidr_blocks"]
-      ipv6_cidr_blocks = ingress.value["ipv6_cidr_blocks"]
+      description      = egress.value["description"]
+      from_port        = egress.value["from_port"]
+      to_port          = egress.value["to_port"]
+      protocol         = egress.value["protocol"]
+      cidr_blocks      = egress.value["cidr_blocks"]
+      ipv6_cidr_blocks = egress.value["ipv6_cidr_blocks"]
     }
   }
 
