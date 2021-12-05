bbae219e49fab94c444a952c2e9bed606a377e83
Sridhar Krishnamurthy <sridhar.krishnamurthy@sage.com>
2021-12-05 12:17:55 -0800

    Output public route table ids

--- terraform-modules/aws/vpc/outputs.tf
+++ terraform-modules/aws/vpc/outputs.tf
@@ -23,10 +23,12 @@ output "k8s_subnets" {
   value       = module.vpc.elasticache_subnets
 }
 
-#
-# Output private_route_table_ids
-#
 output "private_route_table_ids" {
   description = "List of IDs of private route tables"
   value       = module.vpc.private_route_table_ids
+}
+
+output "public_route_table_ids" {
+  description = "List of IDs of public route tables"
+  value       = module.vpc.public_route_table_ids
 }
