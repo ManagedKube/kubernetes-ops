318ba328d1d16d1bf4714235b6e49282a1047d7a
Garland Kan <garland@managedkube.com>
2021-12-06 18:10:35 -0800

    Mongo aws secret (#223)

--- terraform-modules/aws/mongodb-atlas/main.tf
+++ terraform-modules/aws/mongodb-atlas/main.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "this" {
   vpc_id      = var.vpc_id
 
   dynamic "ingress" {
-    for_each  =  var.ingress_rule
+    for_each = var.ingress_rule
     content {
       description      = ingress.value["description"]
       from_port        = ingress.value["from_port"]
@@ -63,7 +63,7 @@ resource "aws_security_group" "this" {
   }
 
   dynamic "egress" {
-    for_each  =  var.egress_rule
+    for_each = var.egress_rule
     content {
       description      = egress.value["description"]
       from_port        = egress.value["from_port"]
@@ -95,7 +95,7 @@ resource "mongodbatlas_privatelink_endpoint_service" "atlasplink" {
 
 resource "mongodbatlas_database_user" "admin" {
   username           = "admin"
-  password           = var.user_password
+  password           = var.create_aws_secret ? aws_secretsmanager_secret_version.this[0].secret_string : var.user_password
   project_id         = var.mongodbatlas_projectid
   auth_database_name = "admin"
 
@@ -122,7 +122,38 @@ resource "mongodbatlas_database_user" "test" {
   }
 
   scopes {
-    name   = var.cluster_name
+    name = var.cluster_name
     type = "CLUSTER"
   }
+}
+
+################################################
+# AWS Secret
+#
+# Option to add the password into AWS secret
+################################################
+resource "aws_secretsmanager_secret" "this" {
+  count                   = var.create_aws_secret ? 1 : 0
+  name                    = var.aws_secret_name
+  description             = var.aws_secret_description
+  recovery_window_in_days = var.recovery_window_in_days
+  tags                    = var.tags
+}
+
+resource "random_password" "password" {
+  count            = var.create_aws_secret ? 1 : 0
+  length           = 16
+  min_lower        = 2
+  min_numeric      = 2
+  min_special      = 2
+  min_upper        = 2
+  number           = true
+  special          = true
+  override_special = "!@#$%&*()-_=+[]{}<>:?"
+}
+
+resource "aws_secretsmanager_secret_version" "this" {
+  count         = var.create_aws_secret ? 1 : 0
+  secret_id     = aws_secretsmanager_secret.this[0].id
+  secret_string = random_password.password[0].result
 }
