4bc3af1de4c76c4b1d5f42a726d77ee364648408
Garland Kan <garland@managedkube.com>
2022-01-03 19:19:54 -0800

    AWS/Github OIDC Federation permissions (#235)

---
+++ terraform-modules/aws/github-oidc-provider/variables.tf
@@ -0,0 +1,50 @@
+variable "name" {
+  description = "The name for the various resources"
+  default     = "github_oidc"
+}
+
+variable "url" {
+  default = "https://token.actions.githubusercontent.com"
+}
+
+variable "client_id_list" {
+  default = [
+    "sts.amazonaws.com"
+  ]
+}
+
+# This is the thumbprint returned if you were to create an "identity provider" in AWS and gave
+# it this url: https://token.actions.githubusercontent.com
+variable "thumbprint_list" {
+  default = [
+    "a031c46782e6e6c662c2c87c76da9aa62ccabd8e"
+  ]
+}
+
+variable "aws_policy_json" {
+  description = "The AWS policy in a json format"
+  default     = <<-EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "*",
+        "Resource": "*"
+      }
+    ]
+}
+EOT
+}
+
+variable "validate_conditions" {
+  description = "Conditions to validate"
+  type        = set(string)
+  default     = ["repo:octo-org/octo-repo:ref:refs/heads/octo-branch"]
+}
+
+variable "tags" {
+  type        = map(any)
+  default     = {}
+  description = "Tags"
+}
