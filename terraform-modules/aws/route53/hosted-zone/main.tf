3d652f7705cfff0fdd60795e9e8374143113cb6d
Sridhar Krishnamurthy <sridhar.krishnamurthy@sage.com>
2022-01-19 10:22:31 -0800

    [OPSPROJ-5627] Support custom policy and alias for kms

--- terraform-modules/aws/route53/hosted-zone/main.tf
+++ terraform-modules/aws/route53/hosted-zone/main.tf
@@ -2,49 +2,14 @@ resource "aws_kms_key" "this" {
   customer_master_key_spec = "ECC_NIST_P256"
   deletion_window_in_days  = 7
   key_usage                = "SIGN_VERIFY"
-  policy = jsonencode({
-    Statement = [
-      {
-        Action = [
-          "kms:DescribeKey",
-          "kms:GetPublicKey",
-          "kms:Sign",
-        ],
-        Effect = "Allow"
-        Principal = {
-          Service = "dnssec-route53.amazonaws.com"
-        }
-        Sid      = "Allow Route 53 DNSSEC Service",
-        Resource = "*"
-      },
-      {
-        Action = "kms:CreateGrant",
-        Effect = "Allow"
-        Principal = {
-          Service = "dnssec-route53.amazonaws.com"
-        }
-        Sid      = "Allow Route 53 DNSSEC Service to CreateGrant",
-        Resource = "*"
-        Condition = {
-          Bool = {
-            "kms:GrantIsForAWSResource" = "true"
-          }
-        }
-      },
-      {
-        Action = "kms:*"
-        Effect = "Allow"
-        Principal = {
-          AWS = "*"
-        }
-        Resource = "*"
-        Sid      = "IAM User Permissions"
-      },
-    ]
-    Version = "2012-10-17"
-  })
+  policy                   = var.kms_key_policy
 
   tags = var.tags
+}
+
+resource "aws_kms_alias" "this" {
+  name          = format("%s/%s", "alias", var.kms_alias)
+  target_key_id = aws_kms_key.this.key_id
 }
 
 resource "aws_route53_zone" "this" {
--- terraform-modules/aws/route53/hosted-zone/variables.tf
+++ terraform-modules/aws/route53/hosted-zone/variables.tf
@@ -3,6 +3,17 @@ variable "domain_name" {
     type = string
 }
 
+variable "kms_alias" {
+  description = "kms alias"
+  type        = string
+  default     = "key"
+}
 variable "tags" {
     type = map(any)
+}
+
+variable "kms_key_policy" {
+    description = "JSON policy for kms key"
+    type        = string
+    default     = ""
 }
