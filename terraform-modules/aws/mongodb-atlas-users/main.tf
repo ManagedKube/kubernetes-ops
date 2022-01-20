573b5cc2d0a864fd9c8db5df59ca2034b378d877
gar <garlandk@gmail.com>
2022-01-20 13:19:47 -0800

    Mongo Atlas - IAM Policy

--- terraform-modules/aws/mongodb-atlas-users/main.tf
+++ terraform-modules/aws/mongodb-atlas-users/main.tf
@@ -84,14 +84,14 @@ resource "aws_iam_role" "this" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      # {
+      {
       #   Action = "sts:AssumeRole"
       #   Effect = "Allow"
       #   Sid    = ""
       #   Principal = {
       #     Service = "ec2.amazonaws.com"
       #   }
-      # },
+      },
     ]
   })
 
