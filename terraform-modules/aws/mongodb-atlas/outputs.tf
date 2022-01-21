db9acedc94f4881fd7d42bf3d602484ee8187b03
gar <garlandk@gmail.com>
2022-01-20 16:28:30 -0800

    Mongo outputs

--- terraform-modules/aws/mongodb-atlas/outputs.tf
+++ terraform-modules/aws/mongodb-atlas/outputs.tf
@@ -17,3 +17,12 @@ output "name" {
 output "service_endpoint_dns" {
   value = aws_vpc_endpoint.mongodbatlas.dns_entry[0]["dns_name"]
 }
+
+# https://registry.terraform.io/providers/mongodb/mongodbatlas/latest/docs/resources/cluster#example---return-a-connection-string
+output "connect_string_aws_private_endpoint" {
+    value = lookup(mongodbatlas_cluster.cluster-test.connection_strings[0].private_endpoint[0].srv_connection_string, aws_vpc_endpoint.ptfe_service.id)
+}
+
+output "connect_string_standard" {
+    value = mongodbatlas_cluster.cluster-test.connection_strings[0].standard
+}
