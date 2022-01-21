3e266e167fcfe25d715f101c1069139c46fe3a4f
gar <garlandk@gmail.com>
2022-01-20 16:34:09 -0800

    Fixing resource name

--- terraform-modules/aws/mongodb-atlas/outputs.tf
+++ terraform-modules/aws/mongodb-atlas/outputs.tf
@@ -20,7 +20,7 @@ output "service_endpoint_dns" {
 
 # https://registry.terraform.io/providers/mongodb/mongodbatlas/latest/docs/resources/cluster#example---return-a-connection-string
 output "connect_string_aws_private_endpoint" {
-    value = lookup(mongodbatlas_cluster.cluster.connection_strings[0].private_endpoint[0].srv_connection_string, aws_vpc_endpoint.ptfe_service.id)
+    value = lookup(mongodbatlas_cluster.cluster.connection_strings[0].private_endpoint[0].srv_connection_string, aws_vpc_endpoint.mongodbatlas.id)
 }
 
 output "connect_string_standard" {
