bfb5ddc4d9059e3f3ff511e5a76488dde3a170a6
gar <garlandk@gmail.com>
2022-01-25 20:16:14 -0800

    Updating var usage

--- terraform-modules/generate-cert/main.tf
+++ terraform-modules/generate-cert/main.tf
@@ -22,9 +22,9 @@ resource "tls_self_signed_cert" "ca" {
   }
 
   # Store the CA public key in a file.
-  provisioner "local-exec" {
-    command = "echo '${tls_self_signed_cert.ca.cert_pem}' > '${var.ca_public_key_file_path}' && chmod ${var.permissions} '${var.ca_public_key_file_path}' && chown ${var.owner} '${var.ca_public_key_file_path}'"
-  }
+  # provisioner "local-exec" {
+  #   command = "echo '${tls_self_signed_cert.ca.cert_pem}' > '${var.ca_public_key_file_path}' && chmod ${var.permissions} '${var.ca_public_key_file_path}' && chown ${var.owner} '${var.ca_public_key_file_path}'"
+  # }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -37,9 +37,9 @@ resource "tls_private_key" "cert" {
   rsa_bits    = "${var.private_key_rsa_bits}"
 
   # Store the certificate's private key in a file.
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.cert.private_key_pem}' > '${var.private_key_file_path}' && chmod ${var.permissions} '${var.private_key_file_path}' && chown ${var.owner} '${var.private_key_file_path}'"
-  }
+  # provisioner "local-exec" {
+  #   command = "echo '${tls_private_key.cert.private_key_pem}' > '${var.private_key_file_path}' && chmod ${var.permissions} '${var.private_key_file_path}' && chown ${var.owner} '${var.private_key_file_path}'"
+  # }
 }
 
 resource "tls_cert_request" "cert" {
@@ -66,7 +66,7 @@ resource "tls_locally_signed_cert" "cert" {
   allowed_uses          = var.allowed_uses
 
   # Store the certificate's public key in a file.
-  provisioner "local-exec" {
-    command = "echo '${tls_locally_signed_cert.cert.cert_pem}' > '${var.public_key_file_path}' && chmod ${var.permissions} '${var.public_key_file_path}' && chown ${var.owner} '${var.public_key_file_path}'"
-  }
+  # provisioner "local-exec" {
+  #   command = "echo '${tls_locally_signed_cert.cert.cert_pem}' > '${var.public_key_file_path}' && chmod ${var.permissions} '${var.public_key_file_path}' && chown ${var.owner} '${var.public_key_file_path}'"
+  # }
 }
--- terraform-modules/generate-cert/outputs.tf
+++ terraform-modules/generate-cert/outputs.tf
@@ -1,11 +1,11 @@
-output "ca_public_key_file_path" {
-  value = "${var.ca_public_key_file_path}"
+output "ca_public_key" {
+  value = tls_self_signed_cert.ca.cert_pem
 }
 
-output "public_key_file_path" {
-  value = "${var.public_key_file_path}"
+output "public_key" {
+  value = tls_locally_signed_cert.cert.cert_pem
 }
 
-output "private_key_file_path" {
-  value = "${var.private_key_file_path}"
+output "private_key" {
+  value = tls_private_key.cert.private_key_pem
 }
