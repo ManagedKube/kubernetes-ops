54a316f5647327de0a0b9b2d054267a5c720941a
Sridhar Krishnamurthy <sridhar.krishnamurthy@sage.com>
2022-02-07 18:02:38 -0800

    [OPSPROJ-6157] Output network interface ID

--- terraform-modules/aws/ec2/outputs.tf
+++ terraform-modules/aws/ec2/outputs.tf
@@ -2,4 +2,8 @@ output "id" {
   description = "The ID of the instance"
   value       = try(module.aws_instance.id, "")
 }
+output "network_interface_id" {
+  description = "The ID of the attached network interface"
+  value       = try(aws_network_interface.this[0].id, "")
+}
 
