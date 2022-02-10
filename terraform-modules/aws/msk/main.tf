c6b174050585ae23102072ff732e7a58b26910aa
Garland Kan <garland@managedkube.com>
2022-02-10 15:06:36 -0800

    Adding s3 bucket

--- terraform-modules/aws/msk/main.tf
+++ terraform-modules/aws/msk/main.tf
@@ -3,16 +3,87 @@ resource "aws_cloudwatch_log_group" "msk_cloudwatch_log_group" {
   tags = var.tags
 }
 
-# module "msk_log_bucket" {
-#   source                  = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/s3_bucket?ref=v1.0.55"
-#   bucket                  = var.s3_logs_bucket
-#   acl                     = "private"
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+#######################################
+# S3 bucket
+#######################################
+resource "aws_kms_key" "this" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
 
+resource "aws_s3_bucket" "this" {
+  bucket = var.s3_logs_bucket
+  acl    = "private"
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.this.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "acmpca_bucket_access" {
+  statement {
+    actions = [
+      "s3:GetBucketAcl",
+      "s3:GetBucketLocation",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+    ]
+
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+
+    principals {
+      identifiers = ["acm-pca.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.acmpca_bucket_access.json
+}
+
+#######################################
+# Private CA
+#######################################
+resource "aws_acmpca_certificate_authority" "this" {
+  certificate_authority_configuration {
+    key_algorithm     = var.key_algorithm
+    signing_algorithm = var.signing_algorithm
+
+    subject {
+      common_name = var.common_name
+    }
+  }
+
+  revocation_configuration {
+    crl_configuration {
+      custom_cname       = "crl.${var.common_name}"
+      enabled            = true
+      expiration_in_days = var.expiration_in_days
+      s3_bucket_name     = aws_s3_bucket.this.id
+    }
+  }
+  
+  tags   = var.tags
+
+  depends_on = [aws_s3_bucket_policy.this]
+}
+
+#######################################
+# MSK Cluster
+#######################################
 module "msk" {
   source                         = "cloudposse/msk-apache-kafka-cluster/aws"
   version                        = "v0.8.3"
@@ -36,13 +107,13 @@ module "msk" {
   cloudwatch_logs_log_group      = var.cloudwatch_logs_enabled == true ? var.cloudwatch_logs_log_group : ""
   enhanced_monitoring            = var.enhanced_monitoring
   node_exporter_enabled          = var.node_exporter_enabled
-  s3_logs_bucket                 = var.s3_logs_enabled == true ? var.s3_logs_bucket : ""
+  s3_logs_bucket                 = var.s3_logs_enabled == true ? aws_s3_bucket.this.id : ""
   s3_logs_enabled                = var.s3_logs_enabled
   s3_logs_prefix                 = var.s3_logs_enabled == true ? var.s3_logs_prefix : ""
 
   depends_on = [
     aws_cloudwatch_log_group.msk_cloudwatch_log_group,
-#     module.msk_log_bucket
+    aws_s3_bucket.this
   ]
 }
 
