d30c2544221ae0356ec529ce585f7e4afb276df8
Garland Kan <garland@managedkube.com>
2022-02-10 14:21:37 -0800

    MSK updates

--- terraform-modules/aws/msk/main.tf
+++ terraform-modules/aws/msk/main.tf
@@ -8,7 +8,7 @@ resource "aws_cloudwatch_log_group" "msk_cloudwatch_log_group" {
 }
 
 module "msk_log_bucket" {
-  source                  = "git::git@github.com:managedkube/kubernetes-ops.git//terraform-modules/aws/s3_bucket?ref=v0.0.8"
+  source                  = "git::git@github.com:managedkube/kubernetes-ops.git//terraform-modules/aws/s3_bucket?ref=v1.0.55"
   bucket                  = var.s3_logs_bucket
   acl                     = "private"
   block_public_acls       = true
