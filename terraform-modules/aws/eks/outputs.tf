939653d5ff651637604c8974c9d038857e41e1c0
mybarretto <mybarretto@users.noreply.github.com>
2022-02-10 10:23:21 -0800

    Exposing IAM Role ARNs as outputs of the EKS module (#250)

--- terraform-modules/aws/eks/outputs.tf
+++ terraform-modules/aws/eks/outputs.tf
@@ -31,6 +31,14 @@ output "worker_security_group_id" {
   value = module.eks.worker_security_group_id
 }
 
+output "cluster_iam_role_arn" {
+  value = module.eks.cluster_iam_role_arn
+}
+
+output "worker_iam_role_arn" {
+  value = module.eks.worker_iam_role_arn
+}
+
 output "cluster_arn" {
   value = module.eks.cluster_arn
 }
