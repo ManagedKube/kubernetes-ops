462c97e7b01b377d0598b337994155a5e5137659
Garland Kan <garland@managedkube.com>
2022-02-11 19:07:33 +0000

    Adding templated istio ingress file

--- terraform-modules/aws/istio/main.tf
+++ terraform-modules/aws/istio/main.tf
@@ -43,6 +43,14 @@ resource "helm_release" "helm_chart_istio_discovery" {
   ]
 }
 
+data "template_file" "helm_chart_istio_ingress" {
+  template = file("${path.module}/values/istio_ingress_values.tpl.yaml")
+
+  vars = {
+    acmARN      = module.acm_request_certificate[0].arn
+  }
+}
+
 resource "helm_release" "helm_chart_istio_ingress" {
   count            = var.install_helm_chart_istio_ingress
   chart            = "${path.module}/istio-${var.istio_version}/manifests/charts/gateways/istio-ingress"
@@ -52,6 +60,7 @@ resource "helm_release" "helm_chart_istio_ingress" {
   verify           = var.verify
 
   values = [
+    data.template_file.helm_chart_istio_ingress.rendered,
     var.helm_values_istio_ingress,
   ]
 
