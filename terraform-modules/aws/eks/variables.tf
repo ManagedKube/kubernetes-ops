4205c8c2cdc8a154022d43305c4181642a814912
Garland Kan <garland.kan@MacBook-Pro.local>
2022-02-17 18:16:27 -0800

    Creating aws config

--- terraform-modules/aws/eks/variables.tf
+++ terraform-modules/aws/eks/variables.tf
@@ -121,3 +121,15 @@ variable "cluster_security_group_additional_rules" {
   type        = any
   default     = {}
 }
+
+variable "manage_aws_auth" {
+  description = "Applies the aws_auth into the EKS cluster"
+  type        = bool
+  default     = true
+}
+
+variable "aws_auth_additional_labels" {
+  description = "Additional kubernetes labels applied on aws-auth ConfigMap"
+  default     = {}
+  type        = map(string)
+}
