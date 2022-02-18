e1c008655286428822be23950e3f683d18e62c14
Garland Kan <garland.kan@macbook-pro.lan>
2022-02-18 06:59:49 -0800

    Removing kubectl apply and enabling kube manifest for import

--- terraform-modules/aws/eks/main.tf
+++ terraform-modules/aws/eks/main.tf
@@ -25,46 +25,46 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
-locals {
-  kubeconfig = yamlencode({
-    apiVersion      = "v1"
-    kind            = "Config"
-    current-context = "terraform"
-    clusters = [{
-      name = module.eks.cluster_id
-      cluster = {
-        certificate-authority-data = module.eks.cluster_certificate_authority_data
-        server                     = module.eks.cluster_endpoint
-      }
-    }]
-    contexts = [{
-      name = "terraform"
-      context = {
-        cluster = module.eks.cluster_id
-        user    = "terraform"
-      }
-    }]
-    users = [{
-      name = "terraform"
-      user = {
-        token = data.aws_eks_cluster_auth.cluster.token
-      }
-    }]
-  })
+# locals {
+#   kubeconfig = yamlencode({
+#     apiVersion      = "v1"
+#     kind            = "Config"
+#     current-context = "terraform"
+#     clusters = [{
+#       name = module.eks.cluster_id
+#       cluster = {
+#         certificate-authority-data = module.eks.cluster_certificate_authority_data
+#         server                     = module.eks.cluster_endpoint
+#       }
+#     }]
+#     contexts = [{
+#       name = "terraform"
+#       context = {
+#         cluster = module.eks.cluster_id
+#         user    = "terraform"
+#       }
+#     }]
+#     users = [{
+#       name = "terraform"
+#       user = {
+#         token = data.aws_eks_cluster_auth.cluster.token
+#       }
+#     }]
+#   })
 
-  current_auth_configmap = yamldecode(module.eks.aws_auth_configmap_yaml)
+#   current_auth_configmap = yamldecode(module.eks.aws_auth_configmap_yaml)
 
-  updated_auth_configmap_data = {
-    data = {
-      mapRoles = yamlencode(
-        distinct(concat(
-        yamldecode(local.current_auth_configmap.data.mapRoles), var.map_roles, )
-      ))
-      mapUsers = yamlencode(var.map_users)
-    }
-  }
+#   updated_auth_configmap_data = {
+#     data = {
+#       mapRoles = yamlencode(
+#         distinct(concat(
+#         yamldecode(local.current_auth_configmap.data.mapRoles), var.map_roles, )
+#       ))
+#       mapUsers = yamlencode(var.map_users)
+#     }
+#   }
 
-}
+# }
 
 # resource "null_resource" "patch_aws_auth_configmap" {
 #   triggers = {
@@ -81,10 +81,10 @@ locals {
 #   }
 # }
 
-output "tmp" {
-  value = local.kubeconfig
-  sensitive = true
-}
+# output "tmp" {
+#   value = local.kubeconfig
+#   sensitive = true
+# }
 
 resource "aws_kms_key" "eks" {
   description = "EKS Secret Encryption Key"
