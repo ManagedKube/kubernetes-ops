9940cd5da43eb9a6afc9bd0ff26c9f77d1bdccfc
Garland Kan <garland.kan@MacBook-Pro.local>
2022-02-18 17:09:01 -0800

    Removing unused vars

--- terraform-modules/aws/eks/variables.tf
+++ terraform-modules/aws/eks/variables.tf
@@ -127,15 +127,3 @@ variable "cluster_security_group_additional_rules" {
   type        = any
   default     = {}
 }
-
-variable "manage_aws_auth" {
-  description = "Applies the aws_auth into the EKS cluster"
-  type        = bool
-  default     = true
-}
-
-variable "aws_auth_additional_labels" {
-  description = "Additional kubernetes labels applied on aws-auth ConfigMap"
-  default     = {}
-  type        = map(string)
-}
