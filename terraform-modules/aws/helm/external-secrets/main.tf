bfd8dd29cba2a58599cd3328caa9cdc93b878416
Garland Kan <garland.kan@MacBook-Pro.local>
2022-02-22 13:14:52 -0800

    Adding creating secret store crd

--- terraform-modules/aws/helm/external-secrets/main.tf
+++ terraform-modules/aws/helm/external-secrets/main.tf
@@ -66,3 +66,32 @@ resource "helm_release" "helm_chart" {
     module.iam_assumable_role_admin
   ]
 }
+
+resource "kubernetes_manifest" "secret_store" {
+  manifest = {
+    "apiVersion" = "external-secrets.io/v1alpha1"
+    "kind"       = "SecretStore"
+    "metadata" = {
+      "name"      = "secretstore-sample"
+      "namespace" = var.namespace
+      "labels"    = {
+        "managed/by": "terraform"
+      }
+    }
+    "spec" = {
+      "provider" = {
+        "aws": {
+          "service": "SecretsManager"
+          "region": data.aws_region.current.name
+          "auth": {
+            "jwt": {
+              "serviceAccountRef": {
+                "name": "${local.base_name}-${var.environment_name}"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
