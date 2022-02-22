8fc3c2facb470219353df85af22a03e1abddfdec
Garland Kan <garland.kan@MacBook-Pro.local>
2022-02-22 14:32:21 -0800

    Updating secret store

--- terraform-modules/aws/helm/external-secrets/install/main.tf
+++ terraform-modules/aws/helm/external-secrets/install/main.tf
@@ -74,7 +74,7 @@ resource "kubernetes_manifest" "secret_store" {
     # "kind"       = "ClusterSecretStore"
     "metadata" = {
       "name"      = "secretstore-main"
-      "namespace" = var.namespace
+      "namespace" = "istio-system" #var.namespace
       "labels"    = {
         "managed/by": "terraform"
       }
