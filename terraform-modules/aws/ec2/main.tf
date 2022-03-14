ffe61dc208fe40e2619256c5900ebb5d4c0207e6
Sridhar Krishnamurthy <sridhar.krishnamurthy@sage.com>
2022-03-14 07:46:25 -0700

    OPSPROJ-6767: Add sg and route configuration along with ec2 provisioning

--- terraform-modules/aws/ec2/main.tf
+++ terraform-modules/aws/ec2/main.tf
@@ -22,10 +22,10 @@ module "aws_instance" {
 resource "aws_network_interface" "this" {
   count = var.create_eni ? 1 : 0
 
-  subnet_id       = var.subnet_id
-  private_ips     = var.secondary_ips
-  security_groups = var.vpc_security_group_ids
-  source_dest_check      = var.source_dest_check
+  subnet_id         = var.subnet_id
+  private_ips       = var.secondary_ips
+  security_groups   = [module.aws_security_group.security_group_id]
+  source_dest_check = var.source_dest_check
 
   tags = var.tags
 
@@ -33,4 +33,36 @@ resource "aws_network_interface" "this" {
     instance     = module.aws_instance.id
     device_index = 1
   }
+}
+
+module "aws_security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "v4.9.0"
+
+  create_sg   = var.create_sg
+  name        = var.sg_name
+  description = var.sg_description
+  vpc_id      = var.vpc_id
+
+  ingress_cidr_blocks      = var.ingress_cidr_blocks
+  ingress_rules            = var.ingress_rules
+  ingress_with_cidr_blocks = var.ingress_with_cidr_blocks
+
+  egress_cidr_blocks      = var.egress_cidr_blocks
+  egress_rules            = var.egress_rules
+  egress_with_cidr_blocks = var.egress_with_cidr_blocks
+}
+
+resource "aws_route" "private_instance_route" {
+  count                  = length(var.instance_destination_cidr_blocks)
+  route_table_id         = var.private_route_table_ids[0]
+  destination_cidr_block = element(var.instance_destination_cidr_blocks, count.index)
+  network_interface_id   = aws_network_interface.this[0].id
+}
+
+resource "aws_route" "public_instance_route" {
+  count                  = length(var.instance_destination_cidr_blocks)
+  route_table_id         = var.public_route_table_ids[0]
+  destination_cidr_block = element(var.instance_destination_cidr_blocks, count.index)
+  network_interface_id   = aws_network_interface.this[0].id
 }
--- terraform-modules/aws/ec2/variables.tf
+++ terraform-modules/aws/ec2/variables.tf
@@ -87,3 +87,92 @@ variable "root_block_device" {
   type        = list(any)
   default     = []
 }
+
+variable "create_sg" {
+  description = "Whether to create security group"
+  type        = bool
+  default     = false
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC where to create security group"
+  type        = string
+  default     = null
+}
+
+variable "sg_name" {
+  description = "Name of security group - not required if create_sg is false"
+  type        = string
+  default     = null
+}
+
+variable "sg_description" {
+  description = "Description of security group"
+  type        = string
+  default     = "Security Group managed by Terraform"
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules to create by name"
+  type        = list(string)
+  default     = []
+}
+
+variable "ingress_cidr_blocks" {
+  description = "List of IPv4 CIDR ranges to use on all ingress rules"
+  type        = list(string)
+  default     = []
+}
+
+variable "ingress_with_cidr_blocks" {
+  description = "List of ingress rules to create where 'cidr_blocks' is used"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "egress_rules" {
+  description = "List of egress rules to create by name"
+  type        = list(string)
+  default     = []
+}
+
+variable "egress_cidr_blocks" {
+  description = "List of IPv4 CIDR ranges to use on all egress rules"
+  type        = list(string)
+  default     = []
+}
+
+variable "egress_with_cidr_blocks" {
+  description = "List of egress rules to create where 'cidr_blocks' is used"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "public_route_table_ids" {
+  description = "Route table ID where the route will be created"
+  type        = list(string)
+  default     = []
+}
+variable "private_route_table_ids" {
+  description = "Route table ID where the route will be created"
+  type        = list(string)
+  default     = []
+}
+
+variable "instance_id" {
+  description = "Instance ID of the Jump host"
+  type        = string
+  default     = ""
+}
+
+variable "network_interface_id" {
+  description = "Network interface ID of host for route"
+  type        = string
+  default     = ""
+}
+
+variable "instance_destination_cidr_blocks" {
+  description = "List of destination cidr blocks for jump host"
+  type        = list(string)
+  default     = []
+}
