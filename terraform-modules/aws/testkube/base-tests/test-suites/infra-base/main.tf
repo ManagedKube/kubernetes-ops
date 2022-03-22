3bb1e6832e8233b555bdcbe02e0662ae4a8a7e75
Garland Kan <garland@managedkube.com>
2022-03-22 18:45:32 +0000

    Removing commented out code

--- terraform-modules/aws/testkube/base-tests/test-suites/infra-base/main.tf
+++ terraform-modules/aws/testkube/base-tests/test-suites/infra-base/main.tf
@@ -4,25 +4,3 @@ module "manifest_set" {
     upload_source_path = path.cwd
     upload_directory   = "yaml"
 }
-
-# resource "kubernetes_manifest" "this" {
-#   for_each = fileset(var.upload_directory, "**/*.*")
-#   manifest = yamldecode(file("${var.upload_source_path}/${var.upload_directory}/${each.value}"))
-# }
-
-# variable "upload_directory" {
-#   type        = string
-#   default     = "yaml"
-#   description = "description"
-# }
-
-
-# resource "kubernetes_manifest" "this" {
-#   for_each = fileset(var.upload_directory, "**/*.*")
-#   manifest = yamldecode(file("${path.cwd}/${var.upload_directory}/${each.value}"))
-# }
-
-# resource "kubernetes_manifest" "that" {
-# #   for_each = fileset(var.upload_directory, "**/*.*")
-#   manifest = yamldecode(file("${path.cwd}/yaml/ts.yaml"))
-# }
