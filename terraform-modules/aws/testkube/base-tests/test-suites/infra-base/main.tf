de79dfe7ee1f17638a7ceb9a4ce403cfb40d3c1b
Garland Kan <garland@managedkube.com>
2022-03-22 04:16:44 +0000

    debug

--- terraform-modules/aws/testkube/base-tests/test-suites/infra-base/main.tf
+++ terraform-modules/aws/testkube/base-tests/test-suites/infra-base/main.tf
@@ -1,12 +1,12 @@
-# module "manifest_set" {
-#     source = "github.com/ManagedKube/kubernetes-ops.git//terraform-modules/aws/kubernetes/manifest_set?ref=gha-testkube"
+module "manifest_set" {
+    source = "github.com/ManagedKube/kubernetes-ops.git//terraform-modules/aws/kubernetes/manifest_set?ref=gha-testkube"
 
-#     upload_directory = "${path.module}/yaml"
-# }
-
-
-resource "kubernetes_manifest" "this" {
-  for_each = fileset("./yaml", "*")
-  manifest = yamldecode(file(each.value))
+    upload_directory = "${path.module}/yaml"
 }
+
+
+# resource "kubernetes_manifest" "this" {
+#   for_each = fileset("./yaml", "*")
+#   manifest = yamldecode(file(each.value))
+# }
 
