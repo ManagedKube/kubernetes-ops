a8401861fa9f12f1368784a4fda687c4ad86ee8b
Garland Kan <garland@managedkube.com>
2022-03-22 04:01:00 +0000

    debug

--- terraform-modules/aws/kubernetes/manifest_set/main.tf
+++ terraform-modules/aws/kubernetes/manifest_set/main.tf
@@ -1,5 +1,5 @@
 # Uploads all of the files in the pass in dir path
 resource "kubernetes_manifest" "this" {
-  for_each = fileset(var.upload_directory, "**/*.*")
+  for_each = fileset(var.upload_directory, "*")
   manifest = yamldecode(tostring(each.value))
 }
--- terraform-modules/aws/testkube/base-tests/test-suites/infra-base/main.tf
+++ terraform-modules/aws/testkube/base-tests/test-suites/infra-base/main.tf
@@ -1,5 +1,5 @@
 module "manifest_set" {
     source = "github.com/ManagedKube/kubernetes-ops.git//terraform-modules/aws/kubernetes/manifest_set?ref=gha-testkube"
 
-    upload_directory = "./yaml/ts.yaml"
+    upload_directory = "./yaml"
 }
