88391422ec3239a6afad13ea0aad6739dd09510b
Garland Kan <garland@managedkube.com>
2022-03-24 00:29:23 +0000

    Testing templating

--- terraform-modules/aws/testkube/base-tests/test-suites/infra-base/main.tf
+++ terraform-modules/aws/testkube/base-tests/test-suites/infra-base/main.tf
@@ -1,5 +1,5 @@
 module "manifest_set" {
-    source = "github.com/ManagedKube/kubernetes-ops.git//terraform-modules/aws/kubernetes/manifest_set?ref=v2.0.11"
+    source = "github.com/ManagedKube/kubernetes-ops.git//terraform-modules/aws/kubernetes/manifest_set?ref=manifest_set"
 
     upload_source_path = path.cwd
     upload_directory   = "yaml"
