097c8eaa46e2daa3924d9774c38501beb246c3bf
Garland Kan <garland@managedkube.com>
2022-04-12 04:25:40 +0000

    Adding global notification setting options

--- terraform-modules/aws/mongodb-atlas-alerts/variables.tf
+++ terraform-modules/aws/mongodb-atlas-alerts/variables.tf
@@ -5,7 +5,7 @@ variable "mongodbatlas_projectid" {
 
 variable "use_global_notification_settings" {
   type        = bool
-  default     = false
+  default     = true
   description = "This will override all notification settings with the global_notification_settings variable"
 }
 
