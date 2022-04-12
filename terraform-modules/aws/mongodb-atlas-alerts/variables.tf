0c9222056aee859eb6eec150d66053e423bea2ab
Garland Kan <garland@managedkube.com>
2022-04-12 04:20:30 +0000

    Adding global notification setting options

--- terraform-modules/aws/mongodb-atlas-alerts/variables.tf
+++ terraform-modules/aws/mongodb-atlas-alerts/variables.tf
@@ -175,13 +175,13 @@ variable "default_alerts" {
           email_enabled = true
           roles = ["GROUP_DATA_ACCESS_READ_ONLY", "GROUP_CLUSTER_MANAGER", "GROUP_DATA_ACCESS_ADMIN"]
         },
-        {
-          type_name     = "ORG"
-          interval_min  = 5
-          delay_min     = 0
-          sms_enabled   = true
-          email_enabled = false
-        },
+        # {
+        #   type_name     = "ORG"
+        #   interval_min  = 5
+        #   delay_min     = 0
+        #   sms_enabled   = true
+        #   email_enabled = false
+        # },
       ]
       matcher = {
         field_name = "HOSTNAME_AND_PORT"
