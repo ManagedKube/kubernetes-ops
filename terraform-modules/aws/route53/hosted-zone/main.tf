3187a1b89ae33fbdb9160be48b580d676cf0974d
Sridhar Krishnamurthy <sridhar.krishnamurthy@sage.com>
2022-05-18 17:42:57 -0700

    OPSPROJ-7957: Change zone_id ref in records

--- terraform-modules/aws/route53/hosted-zone/main.tf
+++ terraform-modules/aws/route53/hosted-zone/main.tf
@@ -1,3 +1,8 @@
+locals {
+  zone_name = sort(keys(module.zones.route53_zone_zone_id))[0]
+  #  zone_id = module.zones.route53_zone_zone_id["app.terraform-aws-modules-example.com"]
+}
+
 module "zones" {
   source  = "terraform-aws-modules/route53/aws//modules/zones"
   version = "v2.6.0"
@@ -12,9 +17,10 @@ module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "v2.6.0"
 
+  zone_name = local.zone_name
+  #  zone_id = local.zone_id
+
   create       = var.create_records
-  zone_id      = values(module.zones.route53_zone_zone_id)[0]
-  zone_name    = values(module.zones.route53_zone_name)[0]
   private_zone = var.private_zone
   records      = var.records
   depends_on   = [module.zones]
