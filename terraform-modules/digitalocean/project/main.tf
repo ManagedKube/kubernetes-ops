97815039c964bdb0993500f907ee7c9a7c5d1d16
bcarranza <bayron.carranza@gmail.com>
2022-05-21 19:32:26 -0600

    token

--- terraform-modules/digitalocean/project/main.tf
+++ terraform-modules/digitalocean/project/main.tf
@@ -6,6 +6,13 @@ terraform {
     }
   }
 }
+variable "do_token" {}
+variable "pvt_key" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
 resource "digitalocean_project" "project" {
   name        = var.name
   description = var.description
