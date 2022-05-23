6a850637b0ef2c10a318a7b940a608b07b2adc1b
bcarranza <bayron.carranza@gmail.com>
2022-05-22 18:37:02 -0600

    ssh key bugfix

--- terraform-modules/digitalocean/project/main.tf
+++ terraform-modules/digitalocean/project/main.tf
@@ -10,10 +10,10 @@ variable "do_token" {}
 variable "pvt_key" {}
 
 provider "digitalocean" {
-  token = "dop_v1_f06110b92b2c712c88467b0982daef5f11b80eff9ce1bc612ab5e3cda3b3a84c"
+  token = var.do_token
 }
 data "digitalocean_ssh_key" "terraform" {
-  name = "46:6e:72:9e:9e:48:8c:cf:df:9b:b7:f7:a2:89:28:3c"
+  name = "digitalocean"
 }
 
 resource "digitalocean_project" "project" {
