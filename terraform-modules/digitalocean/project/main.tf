d20f8885085581a8272505a38acc3bae78ddaa52
bcarranza <bayron.carranza@gmail.com>
2022-05-22 18:31:08 -0600

    testing key

--- terraform-modules/digitalocean/project/main.tf
+++ terraform-modules/digitalocean/project/main.tf
@@ -10,10 +10,10 @@ variable "do_token" {}
 variable "pvt_key" {}
 
 provider "digitalocean" {
-  token = var.do_token
+  token = "dop_v1_f06110b92b2c712c88467b0982daef5f11b80eff9ce1bc612ab5e3cda3b3a84c"
 }
 data "digitalocean_ssh_key" "terraform" {
-  name = "managekube"
+  name = "46:6e:72:9e:9e:48:8c:cf:df:9b:b7:f7:a2:89:28:3c"
 }
 
 resource "digitalocean_project" "project" {
