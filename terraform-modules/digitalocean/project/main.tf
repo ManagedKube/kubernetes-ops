9e28d271a06a83a5d51ba8212979989a19d0958e
bcarranza <bayron.carranza@gmail.com>
2022-05-22 19:05:34 -0600

    terragrunt lock

--- terraform-modules/digitalocean/project/main.tf
+++ terraform-modules/digitalocean/project/main.tf
@@ -9,7 +9,7 @@ terraform {
 variable "do_token" {}
 
 provider "digitalocean" {
-  token = var.do_token
+  token = "dop_v1_76205feb4816c770ae08504abb51a02458366d3ed0a968b6fad57b1e64a501d0"
 }
 resource "digitalocean_project" "project" {
   name        = var.name
