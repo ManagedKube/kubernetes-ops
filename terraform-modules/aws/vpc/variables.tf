bddc394b96366b4eae24466f551123dd98dff1d3
Bayron Carranza <bayron.carranza@gmail.com>
2022-05-31 21:39:38 -0600

    CR-150 Security Group Default Management (#307)

--- terraform-modules/aws/vpc/variables.tf
+++ terraform-modules/aws/vpc/variables.tf
@@ -74,3 +74,77 @@ variable "external_nat_ip_ids" {
   type        = list(string)
   default     = []
 }
+
+#Default Security Group Management (Default: secure)
+variable "manage_default_security_group" {
+  description = "Should be true to adopt and manage default security group"
+  type        = bool
+  default     = true
+}
+
+variable "default_security_group_name" {
+  description = "Name to be used on the default security group	"
+  type        = string
+  default     = "default"
+}
+
+variable "default_security_group_egress" {
+  description = "List of maps of egress rules to set on the default security group"
+  type        = list(map(string))
+  default     = [
+    {
+      cidr_blocks = "0.0.0.0/0"
+      description = "Allow all"
+      from_port   = 0
+      protocol    = "-1"
+      self        = false
+      to_port     = 0
+    }
+  ]
+}
+
+variable "default_security_group_ingress" {
+  description = "List of maps of ingress rules to set on the default security group	"
+  type        = list(map(string))
+  default     = [
+    {
+      cidr_blocks = "10.0.0.0/8"
+      description = "rfc1918: Private Address Space"
+      from_port   = 0
+      protocol    = "-1"
+      self        = false
+      to_port     = 0
+    },
+    {
+      cidr_blocks = "172.16.0.0/12"
+      description = "rfc1918: Private Address Space"
+      from_port   = 0
+      protocol    = "-1"
+      self        = false
+      to_port     = 0
+    },
+    {
+      cidr_blocks = "192.168.0.0/16"
+      description = "rfc1918: Private Address Space"
+      from_port   = 0
+      protocol    = "-1"
+      self        = false
+      to_port     = 0
+    },
+    {
+      cidr_blocks = "100.64.0.0/10"
+      description = "rfc6598: Private Address Space"
+      from_port   = 0
+      protocol    = "-1"
+      self        = false
+      to_port     = 0
+    }
+  ]
+}
+
+variable "default_security_group_tags" {
+  description = "Additional tags for the default security group	"
+  type    = map(any)
+  default = {}
+}
+
