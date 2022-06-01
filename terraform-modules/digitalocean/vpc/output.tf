80365db6f1fcc3110a41dd3a1f2fbc72476f97c9
bcarranza <bayron.carranza@gmail.com>
2022-05-31 20:40:41 -0600

    refactor digitalocean_vpc

--- terraform-modules/digitalocean/vpc/output.tf
+++ terraform-modules/digitalocean/vpc/output.tf
@@ -1,12 +1,12 @@
 output "vpc_id" {
   description = "The unique identifier for the VPC."
-  value       = digitalocean_project.vpc.id
+  value       = digitalocean_vpc.vpc.id
 }
 output "vpc_urn" {
   description = "The uniform resource name (URN) for the VPC."
-  value       = digitalocean_project.vpc.urn
+  value       = digitalocean_vpc.vpc.urn
 }
 output "vpc_default" {
   description = "A boolean indicating whether or not the VPC is the default one for the region."
-  value       = digitalocean_project.vpc.default
+  value       = digitalocean_vpc.vpc.default
 }
