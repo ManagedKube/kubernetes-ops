f3f2c7e22d4738b9cba711ee53645aef8e4e7865
Bayron Carranza <bayron.carranza@gmail.com>
2022-06-13 15:16:29 -0600

    bugfix remove module

--- terraform-modules/aws/vpc_flow_log_s3/outputs.tf
+++ terraform-modules/aws/vpc_flow_log_s3/outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_flow_id" {
   description = "The Flow Log ID"
-  value       = module.aws_flow_log.this.id
+  value       = aws_flow_log.this.id
 }
 
 output "vpc_flow_arn" {
   description = "The Flow Log ID"
-  value       = module.aws_flow_log.this.arn
+  value       = aws_flow_log.this.arn
 }
