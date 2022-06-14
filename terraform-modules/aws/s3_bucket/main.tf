0eb7863a7471e318625286afebee4fa50ad30181
gkan <garlandk@gmail.com>
2022-06-14 15:42:24 -0700

    Adding tags to the kms resource

--- terraform-modules/aws/s3_bucket/main.tf
+++ terraform-modules/aws/s3_bucket/main.tf
@@ -1,6 +1,8 @@
 resource "aws_kms_key" "kms_key" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
+
+  tags = var.tags
 }
 
 resource "aws_s3_bucket" "bucket" {
