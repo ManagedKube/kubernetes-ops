ab1b4c2d084a663b5c8af21d3fb3c2ff9c5bec69
Garland Kan <garland@managedkube.com>
2022-06-15 09:40:49 -0700

    aws-s3-encrypt-update-2 (#310)

--- terraform-modules/aws/s3_bucket/main.tf
+++ terraform-modules/aws/s3_bucket/main.tf
@@ -13,12 +13,11 @@ resource "aws_s3_bucket" "bucket" {
   tags = var.tags
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_config" {
   bucket = aws_s3_bucket.bucket.bucket
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.kms_key.arn
       sse_algorithm     = "aws:kms"
     }
   }
