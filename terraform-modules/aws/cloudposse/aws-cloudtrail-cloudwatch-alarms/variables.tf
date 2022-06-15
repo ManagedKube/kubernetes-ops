d5ab7bb39b9cffd4c05bfa126885e6d582650431
Bayron Carranza <bayron.carranza@gmail.com>
2022-06-14 23:30:17 -0600

    testing adding name

--- terraform-modules/aws/cloudposse/aws-cloudtrail-cloudwatch-alarms/variables.tf
+++ terraform-modules/aws/cloudposse/aws-cloudtrail-cloudwatch-alarms/variables.tf
@@ -6,3 +6,8 @@ variable "metrics_paths" {
   type        = list(string)
   description = "List of paths to CloudWatch metrics configurations"
 }
+
+variable "name" {
+  type = string
+  description = "Log group name"
+}
