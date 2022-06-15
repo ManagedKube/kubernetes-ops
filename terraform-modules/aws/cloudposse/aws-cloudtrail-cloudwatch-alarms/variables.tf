f1286fffd2a3b7ca627fc979b91fc51927e5ad29
Bayron Carranza <bayron.carranza@gmail.com>
2022-06-14 23:35:57 -0600

    remove name var

--- terraform-modules/aws/cloudposse/aws-cloudtrail-cloudwatch-alarms/variables.tf
+++ terraform-modules/aws/cloudposse/aws-cloudtrail-cloudwatch-alarms/variables.tf
@@ -6,8 +6,3 @@ variable "metrics_paths" {
   type        = list(string)
   description = "List of paths to CloudWatch metrics configurations"
 }
-
-variable "name" {
-  type = string
-  description = "Log group name"
-}
