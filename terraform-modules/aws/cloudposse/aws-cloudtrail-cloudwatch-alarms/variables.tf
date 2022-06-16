857be77c6f8071e7f6be9bd63dce53f29d2106c3
Bayron Carranza <bayron.carranza@gmail.com>
2022-06-15 19:57:12 -0600

    list

--- terraform-modules/aws/cloudposse/aws-cloudtrail-cloudwatch-alarms/variables.tf
+++ terraform-modules/aws/cloudposse/aws-cloudtrail-cloudwatch-alarms/variables.tf
@@ -8,17 +8,17 @@ variable "metrics_paths" {
 }
 
 variable "cloudtrail_event_selector" {
-  type = list(object({include_management_events = bool, read_write_type = string, data_resource = object({type = string, values = list(string)})}))
+  type = list(object({include_management_events = bool, read_write_type = string, data_resource = list(object({type = string, values = list(string)}))}))
   
   description = "This enables the cloudtrail even selector to track all S3 API calls: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudtrail."
   default = [
     {
       include_management_events = true
-      read_write_type           = "All"
-      data_resource             = {
-        type   = "AWS::S3::Object"
+      read_write_type = "All"
+      data_resource = [{
+        type = "AWS::S3::Object"
         values = ["arn:aws:s3"]
-      }
+      }]
     }
   ]
 }
