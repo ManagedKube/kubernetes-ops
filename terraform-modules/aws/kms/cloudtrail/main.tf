02cf0ef8fc0aa49389c7accfcf42289eea1078ae
Bayron Carranza <bayron.carranza@gmail.com>
2022-07-15 15:49:12 -0600

    fix type in kms alias

--- terraform-modules/aws/kms/cloudtrail/main.tf
+++ terraform-modules/aws/kms/cloudtrail/main.tf
@@ -29,7 +29,7 @@ data "aws_iam_policy_document" "kms" {
 
 resource "aws_kms_alias" "a" {
   name          = "alias/${var.cloudtrail_name}"
-  target_key_id = aws_kms_key.a.key_id
+  target_key_id = aws_kms_key.kms.key_id
 }
 
 resource "aws_kms_key" "kms" {
