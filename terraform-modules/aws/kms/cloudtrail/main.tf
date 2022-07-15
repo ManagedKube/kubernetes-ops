3c2eba333f8b259dc3de98c35dec2c731aef02ce
Bayron Carranza <bayron.carranza@gmail.com>
2022-07-15 16:18:45 -0600

    add kms administrator to root

--- terraform-modules/aws/kms/cloudtrail/main.tf
+++ terraform-modules/aws/kms/cloudtrail/main.tf
@@ -1,10 +1,50 @@
 # This is a standard kms that frees any cloudtrail/trails from vulnerabilities.
 # Docs: https://dev.to/aws-builders/encrypt-cloudtrail-logs-with-multi-region-key-with-terraform-1hln
+
+locals {
+  arn_format  = "arn:${data.aws_partition.current.partition}"
+}
+
 data "aws_partition" "current" {}
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_iam_policy_document" "kms" {
+  statement {
+    sid    = "Enable Root User Permissions"
+    effect = "Allow"
+
+    actions = [
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:Tag*",
+      "kms:Untag*",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
+    ]
+
+    #bridgecrew:skip=CKV_AWS_109:This policy applies only to the key it is attached to
+    #bridgecrew:skip=CKV_AWS_111:This policy applies only to the key it is attached to
+    resources = [
+      "*"
+    ]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "${local.arn_format}:iam::${data.aws_caller_identity.current.account_id}:root"
+      ]
+    }
+  }
   statement {
     sid    = "Allow CloudTrail to encrypt  - ${var.cloudtrail_name}"
     effect    = "Allow"
