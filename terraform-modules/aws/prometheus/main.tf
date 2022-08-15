edf7c85322d2cdd35d2fd0a85db5a5a9fb9b4aef
gkan <garlandk@gmail.com>
2022-08-15 15:50:01 -0700

    making the OIDC provider url dynamic

--- terraform-modules/aws/prometheus/main.tf
+++ terraform-modules/aws/prometheus/main.tf
@@ -34,7 +34,7 @@ module "iam_assumable_role_admin" {
   create_role = true
   role_name   = "${local.base_name}-${var.iam_access_grant_list[count.index].environment_name}"
   # role_path                     = "/token-file-web-identity/"
-  aws_account_id                = data.aws_caller_identity.current.account_id
+  aws_account_id                = data.aws_caller_identity.current[count.index].account_id
 
   # The Statement[].Principal.Federated value in the AWS IAM Role's -> Trust Relationship
   # In this case we are going to set the principal to the OIDC provider we created above which is
