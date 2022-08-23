e20c68bd3d54dc4df601c944a3c97fb604bee9d1
Bayron Carranza <bayron.carranza@gmail.com>
2022-08-23 07:24:38 -0600

    fix variables name

--- terraform-modules/aws/iam/generic/variables.tf
+++ terraform-modules/aws/iam/generic/variables.tf
@@ -1,21 +1,21 @@
-variable "iam_name" {
+variable iam_name {
   type        = string
   description = "Friendly name of the role"
 }
 
-variable "iam_description" {
+variable iam_description {
   type        = string
   default     = "New Role created from ManagedKube Module"
   description = "(Optional) Description of the role."
 }
 
-variable "iam_force_detach_policies" {
+variable iam_force_detach_policies {
   type        = bool
   default     = false
   description = "(Optional) Whether to force detaching any policies the role has before destroying it"
 }
 
-variable "iam_max_session_duration" {
+variable iam_max_session_duration {
   type        = number
   default     = 60
   description = "(Optional) Maximum session duration (in seconds) that you want to set for the specified role his setting can have a value from 1 hour to 12 hours."
@@ -24,13 +24,13 @@ variable "iam_max_session_duration" {
 
 
 #Permission section-----------------------------------------
-variable "iam_inline_policy" {
+variable iam_inline_policy {
   type        = string
   description = "Json to create policy in line"
   default     = "{}"
 }
 
-variable "iam_managed_policy_arns" {
+variable iam_managed_policy_arns {
   type        = list(string)
   description = "List of arn policies to attached"
   default     = []
@@ -39,7 +39,7 @@ variable "iam_managed_policy_arns" {
 
 
 #Trust relationship section---------------------------------
-variable "iam_assume_role_policy " {
+variable iam_assume_role_policy {
   type        = string
   description = "Json to create assume_role_policy in line"
   default     = "{}"
@@ -47,7 +47,7 @@ variable "iam_assume_role_policy " {
 #End Trust relationship section-----------------------------
 
 
-variable "tags" {
+variable tags {
   type = map(any)
   description = "Key-value mapping of tags for the IAM role. If configured with a provider"
 }
