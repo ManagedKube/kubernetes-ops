145e31eb24cf8f1e0ae440da8c4bb5fa2b1ee00d
sakruthiJupalli <sjupalli@exactpay.com>
2022-08-23 23:02:25 -0400

    Added nlb configuration

--- terraform-modules/aws/nlb/main.tf
+++ terraform-modules/aws/nlb/main.tf
@@ -6,6 +6,21 @@ resource "aws_lb" "nlb" {
 
   enable_deletion_protection = var.enable_deletion_protection
 
+  dynamic "access_logs" {
+    # The contents of the list is irrelevant. The only important thing is whether or not to create this block.
+    for_each = var.enable_alb_access_logs ? ["use_access_logs"] : []
+    content {
+      bucket  = var.nlb_access_logs_s3_bucket_name
+      prefix  = var.nlb_access_logs_s3_prefix
+      enabled = true
+    }
+  }
+
+
+ enable_cross_zone_load_balancing = var.enable_cross_zone_load_balancing
+
+ enable_http2 = var.enable_http2
+
   tags = {
     Environment = "Ops"
   }
