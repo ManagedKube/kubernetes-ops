d42a22fa751f275e254c686c1fae026e411c0857
sakruthiJupalli <sjupalli@exactpay.com>
2022-08-31 23:38:42 -0400

    fixed comments and attached terradoc

--- terraform-modules/aws/nlb/main.tf
+++ terraform-modules/aws/nlb/main.tf
@@ -1,12 +1,12 @@
 resource "aws_lb" "nlb" {
   name               = var.nlb_name
-  internal           = var.enable_internal 
+  internal           = var.enable_internal
   load_balancer_type = "network"
   subnets            = var.nlb_subnets
 
   enable_deletion_protection = var.enable_deletion_protection
 
-dynamic "access_logs" {
+  dynamic "access_logs" {
     # The contents of the list is irrelevant. The only important thing is whether or not to create this block.
     for_each = var.enable_nlb_access_logs
     content {
@@ -17,11 +17,9 @@ dynamic "access_logs" {
   }
 
 
- enable_cross_zone_load_balancing = var.enable_cross_zone_load_balancing
+  enable_cross_zone_load_balancing = var.enable_cross_zone_load_balancing
 
- enable_http2 = var.enable_http2
+  enable_http2 = var.enable_http2
 
-  tags = {
-    Environment = "Ops"
-  }
+  tags = var.nlb_tags
 }
