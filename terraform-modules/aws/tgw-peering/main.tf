4af777e8adf7707200faad0982095d6b839f901f
Prasanth Koya <prasanth.koya@sage.com>
2022-10-20 13:07:08 -0500

    tgw-peering with static route and reverse static route module

--- terraform-modules/aws/tgw-peering/main.tf
+++ terraform-modules/aws/tgw-peering/main.tf
@@ -1,7 +1,8 @@
 resource "aws_ec2_transit_gateway_peering_attachment" "this" {
-  count = length(var.peer_transit_gateway_id)
+  count = var.create_peer_tgw_connection ? 1 : 0
   peer_account_id         = var.peer_account_id
-  peer_region             = element(var.peer_region, count.index)
+  peer_region             = var.peer_region
+  #peer_region             = element(var.peer_region, count.index)
   peer_transit_gateway_id = element(var.peer_transit_gateway_id, count.index)
   transit_gateway_id      = var.transit_gateway_id
 
@@ -11,6 +12,28 @@ resource "aws_ec2_transit_gateway_peering_attachment" "this" {
 resource "aws_ec2_transit_gateway_route" "tgw_route" {
   count                          = var.create_tgw_static_route ? length(var.destination_cidr_blocks) : 0
   destination_cidr_block         = element(var.destination_cidr_blocks, count.index)
-  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.this[count.index].id
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.this[0].id
   transit_gateway_route_table_id = var.tgw_route_table_id
 }
+
+provider "aws" {
+  alias = "acceptor"
+  region = var.acceptor_region
+}
+
+
+resource "aws_ec2_transit_gateway_peering_attachment_accepter" "this_acceptor" {
+  count         = var.accept_tgw_peer_attachment ? 1 : 0
+  provider = aws.acceptor
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_peering_attachment.this[0].id
+  tags = var.acceptor_tags
+}
+
+resource "aws_ec2_transit_gateway_route" "tgw_reverse_route" {
+  count                          = var.create_tgw_reverse_static_route ? length(var.destination_reverse_cidr_blocks) : 0
+  provider = aws.acceptor
+  destination_cidr_block         = element(var.destination_reverse_cidr_blocks, count.index)
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.this[0].id
+  transit_gateway_route_table_id = var.tgw_reverse_route_table_id
+}
+
--- terraform-modules/aws/tgw-peering/variables.tf
+++ terraform-modules/aws/tgw-peering/variables.tf
@@ -1,3 +1,9 @@
+variable "create_peer_tgw_connection" {
+  description = "TGW peering connection"
+  type = bool
+  default = true
+}
+
 variable "peer_account_id" {
     description = "defaults to requester account"
     type = string
@@ -5,7 +11,7 @@ variable "peer_account_id" {
 }
 
 variable "peer_region" {
-    type = list(string)
+    type = string
 }
 
 variable "peer_transit_gateway_id" {
@@ -36,6 +42,43 @@ variable "destination_cidr_blocks" {
 
 variable "tgw_route_table_id" {
     description = "TGW default route table ID where the static routes need to be created"
+    type        = string
+    default     = null
+}
+
+variable "acceptor_region" {
+  description = "TGW peering attachment acceptor region"
+  type        = string
+  default     = ""
+  
+}
+
+variable "accept_tgw_peer_attachment" {
+  description = "TGW peering accept"
+  type = bool
+  default = true
+}
+
+variable "acceptor_tags" {
+  description = "tags to apply to resource"
+  type        = map(string)
+  default     = {}
+}
+
+variable "create_tgw_reverse_static_route" {
+  description = "Create tgw reverse static route if TRUE"
+  type        = bool
+  default     = true
+}
+
+variable "destination_reverse_cidr_blocks" {
+  description = "List of destination cidr blocks"
+  type        = list(string)
+  default     = []
+}
+
+variable "tgw_reverse_route_table_id" {
+    description = "acceptor TGW default route table ID where the static routes need to be created"
     type        = string
     default     = null
 }
