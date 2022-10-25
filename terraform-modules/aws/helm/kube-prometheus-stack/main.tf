871138516bf0edff821646ef7fc0dec0452b82a9
garland <garland.kan@anthem.com>
2022-10-25 13:26:25 -0700

    Renaming the AWS IAM policy to kube-prometheus-stack-grafana

--- terraform-modules/aws/helm/kube-prometheus-stack/main.tf
+++ terraform-modules/aws/helm/kube-prometheus-stack/main.tf
@@ -35,10 +35,10 @@ module "iam_assumable_role_grafana" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
   version                       = "3.6.0"
   create_role                   = true
-  role_name                     = "${local.k8s_service_account_name}-${var.environment_name}"
+  role_name                     = "kube-prometheus-stack-grafana" #"${local.k8s_service_account_name}-${var.environment_name}"
   provider_url                  = replace(var.eks_cluster_oidc_issuer_url, "https://", "")
   role_policy_arns              = [aws_iam_policy.grafana[0].arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${local.k8s_service_account_name}-${var.environment_name}"]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:kube-prometheus-stack-grafana"]  #${local.k8s_service_account_name}-${var.environment_name}"]
   tags                          = var.tags
 }
 
