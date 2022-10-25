57ff535ca22e40dc9354937ca1747b677dd617ab
garland <garland.kan@anthem.com>
2022-10-24 19:56:27 -0700

    kube-prom-stack-grafana-iam-policy

--- terraform-modules/aws/helm/kube-prometheus-stack/main.tf
+++ terraform-modules/aws/helm/kube-prometheus-stack/main.tf
@@ -1,3 +1,8 @@
+locals {
+  base_name                = "kube-prometheus-stack"
+  k8s_service_account_name = "kube-prometheus-stack-grafana"
+}
+
 resource "helm_release" "helm_chart" {
   chart            = "kube-prometheus-stack"
   namespace        = var.namespace
@@ -12,4 +17,35 @@ resource "helm_release" "helm_chart" {
     var.helm_values,
   ]
 
+}
+
+############################
+# An AWS assumable role for grafana
+#
+# Use case:
+# * If you want to give Grafana IAM permission to query AWS Cloudwatch logs
+#
+############################
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+module "iam_assumable_role_grafana" {
+  count                         = var.enable_iam_assumable_role_grafana ? 1 : 0
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "3.6.0"
+  create_role                   = true
+  role_name                     = "${local.base_name}-${var.environment_name}"
+  provider_url                  = replace(var.eks_cluster_oidc_issuer_url, "https://", "")
+  role_policy_arns              = [aws_iam_policy.grafana.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${local.k8s_service_account_name}-${var.environment_name}"]
+  tags                          = var.tags
+}
+
+# Policy doc: https://github.com/kubernetes-sigs/aws-efs-csi-driver/blob/master/docs/iam-policy-example.json
+resource "aws_iam_policy" "grafana" {
+  count       = var.enable_iam_assumable_role_grafana ? 1 : 0
+  name_prefix = "${local.base_name}-${var.environment_name}"
+  description = "${local.base_name} for ${var.environment_name}"
+  policy      = var.aws_policy_grafana
+  tags        = var.tags
 }
--- terraform-modules/aws/helm/kube-prometheus-stack/variables.tf
+++ terraform-modules/aws/helm/kube-prometheus-stack/variables.tf
@@ -27,3 +27,45 @@ variable helm_values {
   default     = ""
   description = "Additional helm values to pass in.  These values would override the default in this module."
 }
+
+variable "tags" {
+  type = map(any)
+}
+
+variable "enable_iam_assumable_role_grafana" {
+  type        = bool
+  default     = false
+  description = "Enable the creation of an AWS IAM assumable role that is attached to the Grafana kubernetes service account.  Use case is to give Grafana access to AWS Cloudwatch log via an assumable role."
+}
+
+variable "environment_name" {
+  type        = string
+  default     = "env"
+  description = "An environment name to attach to some resources.  Optional only needed if you are going to create more than one of these items in an AWS account"
+}
+
+# Sample AWS IAM policy: https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/iam-identity-based-access-control-cwl.html#managed-policies-cwl
+variable "aws_policy_grafana" {
+  type        = string
+  default     = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "logs:Describe*",
+                "logs:Get*",
+                "logs:List*",
+                "logs:StartQuery",
+                "logs:StopQuery",
+                "logs:TestMetricFilter",
+                "logs:FilterLogEvents"
+            ],
+            "Effect": "Allow",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+  description = "The AWS policy for the Grafana AWS role.  The default is a read only role to all Cloudwatch logs."
+}
