363b7290cf0371cb9008eda1c9ad8b47f8ae4148
garland <garland.kan@anthem.com>
2022-11-01 05:56:27 -0700

    Setting to sensitive output

--- terraform-modules/azure/aks/cluster/outputs.tf
+++ terraform-modules/azure/aks/cluster/outputs.tf
@@ -3,7 +3,8 @@ output "cluster_id" {
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
+  sensitive = true
+  value     = azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate
 }
 
 output "kube_config" {
