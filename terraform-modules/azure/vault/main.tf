a7b9cf02060c0435429a2bf0c34ad8b3005d0b63
Garland Kan <garland.kan@MacBook-Pro.local>
2022-11-14 11:37:40 -0800

    Adding vault module

---
+++ terraform-modules/azure/vault/main.tf
@@ -0,0 +1,7 @@
+resource "azurerm_key_vault" "this" {
+  name                = var.vault_name
+  location            = var.azure_location
+  resource_group_name = var.resource_group_name
+  tenant_id           = var.tenant_id
+  sku_name            = var.sku_name
+}
---
+++ terraform-modules/azure/vault/variables.tf
@@ -0,0 +1,29 @@
+variable "vault_name" {
+  type = string
+  default = "vault1"
+  description = "(Required) Name of this vault instance"
+}
+
+variable "azure_location" {
+  type = string
+  default = "eastus2"
+  description = "(Required) The Azure location"
+}
+
+variable "resource_group_name" {
+  type = string
+  default = "kubernetes-ops-aks"
+  description = "(Required) The Azure resource group name."
+}
+
+variable "tenant_id" {
+  type = string
+  default = ""
+  description = "(Required) The Azure tentant ID."
+}
+
+variable "sku_name" {
+  type = string
+  default = "standard"
+  description = "(Required) The Name of the SKU used for this Key Vault. Possible values are standard and premium."
+}
