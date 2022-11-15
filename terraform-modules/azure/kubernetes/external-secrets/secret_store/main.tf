24c2ef97f6ad4807c84366a55eb63f2ed36a0d44
Garland Kan <garland.kan@MacBook-Pro.local>
2022-11-14 17:27:14 -0800

    debugging

--- terraform-modules/azure/kubernetes/external-secrets/secret_store/main.tf
+++ terraform-modules/azure/kubernetes/external-secrets/secret_store/main.tf
@@ -60,7 +60,7 @@ resource "kubernetes_manifest" "k8s_service_account" {
   manifest = yamldecode(templatefile("yaml/service_account.yaml", {
     serviceAccountName = "${local.base_name}-${var.environment_name}"
     # # The application service client_id/application_id
-    # client_id          = azuread_application.app.application_id
+    client_id          = azuread_application.app.application_id
     tenant_id          = var.azure_tenant_id
   }))
 
