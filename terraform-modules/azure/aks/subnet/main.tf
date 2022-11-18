036e509a66ce7e04c7a2d8df22c6c0e2edd20270
Garland Kan <garland.kan@MacBook-Pro.local>
2022-11-18 08:53:25 -0800

    Removing old comments

--- terraform-modules/azure/aks/subnet/main.tf
+++ terraform-modules/azure/aks/subnet/main.tf
@@ -1,9 +1,3 @@
-# data "azurerm_subnet" "subnet_1" {
-#   name                 = "SNET-LB-1"
-#   virtual_network_name = "VNET-DEV-EASTUS2-POD301"
-#   resource_group_name  = "RG-DEV-EASTUS2-POD301"
-# }
-
 resource "azurerm_subnet" "this" {
   name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
