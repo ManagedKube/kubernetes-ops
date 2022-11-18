bc52065ad6b78e10c82c2dccd374ebf565aff007
Garland Kan <garland.kan@MacBook-Pro.local>
2022-11-18 08:38:40 -0800

    updating var

--- terraform-modules/azure/aks/subnet/main.tf
+++ terraform-modules/azure/aks/subnet/main.tf
@@ -8,6 +8,6 @@ resource "azurerm_subnet" "this" {
   name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = var.address_prefixes
 
 }
