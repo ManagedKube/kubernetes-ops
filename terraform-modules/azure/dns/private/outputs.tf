5eeae798ba9b0494baee2333a9d4c5f82dbd47ae
Garland Kan <garland.kan@MacBook-Pro.local>
2022-12-05 15:33:00 -0800

    private dns zone

--- terraform-modules/azure/dns/private/outputs.tf
+++ terraform-modules/azure/dns/private/outputs.tf
@@ -1,7 +1,7 @@
 output "zone_id" {
-  value = azurerm_dns_zone.this.id
+  value = azurerm_private_dns_zone.this.id
 }
 
 output "nameservers" {
-  value = azurerm_dns_zone.this.name_servers
+  value = azurerm_private_dns_zone.this.name_servers
 }
