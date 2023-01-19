7d63a54cbe21293fadb7c4f7f481239e27d60d53
garland <garland.kan@anthem.com>
2023-01-19 07:52:46 -0800

    removing kms_master_key_id var

--- terraform-modules/aws/loki-stack/main.tf
+++ terraform-modules/aws/loki-stack/main.tf
@@ -33,7 +33,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
 
   rule {
     apply_server_side_encryption_by_default {
-      kms_master_key_id = "aws/s3" # aws_kms_key.this.arn
+      # kms_master_key_id = aws_kms_key.this.arn
       sse_algorithm     = "AES256" #"aws:kms"
     }
   }
