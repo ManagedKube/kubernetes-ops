54a5c957673879f653d9b4fb9d69a79804b245c5
Garland Kan <garland.kan@MacBook-Pro.local>
2023-01-27 10:42:21 -0800

    updating to module version 6.5.0

--- terraform-modules/azure/aks/cluster2/main.tf
+++ terraform-modules/azure/aks/cluster2/main.tf
@@ -11,7 +11,7 @@ data "azurerm_client_config" "current" {
 
 module "aks_cluster" {
   source  = "Azure/aks/azurerm"
-  version = "6.2.0"
+  version = "6.5.0"
 
   prefix                               = var.dns_prefix
   resource_group_name                  = local.resource_group.name
