d41d456787e40f982754b7e9ee0f86de24a664a1
Garland Kan <garland.kan@MacBook-Pro.local>
2023-02-06 16:41:59 -0800

    Azure Subnet list support

--- terraform-modules/azure/networking/subnet/vars.tf
+++ terraform-modules/azure/networking/subnet/vars.tf
@@ -1,9 +1,3 @@
-variable "subnet_name" {
-  type        = string
-  default     = ""
-  description = "(Required) The name of the subnet. Changing this forces a new resource to be created."
-}
-
 variable "resource_group_name" {
   type        = string
   default     = ""
@@ -16,8 +10,13 @@ variable "virtual_network_name" {
   description = "(Required) The name of the virtual network to which to attach the subnet. Changing this forces a new resource to be created."
 }
 
-variable "address_prefixes" {
-  type        = list
-  default     = ["10.0.1.0/24"]
-  description = "(Required) The address prefixes to use for the subnet."
+variable "subnets" {
+  description = "A list of subnets to create"
+  type        = any
+  default     = [
+    {
+      name           = "SNET-AKS-Private-1"
+      address_prefix = ["10.10.10.0/24"]
+    },
+  ]
 }
