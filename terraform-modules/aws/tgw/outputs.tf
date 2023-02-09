cfb1e128a987980b56a7fa8c0a4a74fcc46a2f97
Prasanth Koya <prasanth.koya@sage.com>
2023-02-09 11:20:52 -0600

    [IAOPSPS-243]-updating the tgw-module for creating the policy table

--- terraform-modules/aws/tgw/outputs.tf
+++ terraform-modules/aws/tgw/outputs.tf
@@ -7,3 +7,9 @@ output "tgw" {
   # value       = module.tgw.ec2_transit_gateway_route_table_id
   value       = module.tgw.*
 }
+
+output "transit_gateway_policy_table_id" {
+  description = "The ID of the policy table"
+  value = resource.aws_ec2_transit_gateway_policy_table.tgw_policy_table.*
+  
+}
