3f60695dcfaeb289b5f1efb2085708633301ee90
Garland Kan <garland.kan@MacBook-Pro.local>
2023-02-09 15:09:10 -0800

    Azure Container Registry

--- terraform-modules/azure/container_registry/main.tf
+++ terraform-modules/azure/container_registry/main.tf
@@ -25,16 +25,47 @@ resource "azurerm_container_registry" "acr" {
     identity_client_id = azurerm_user_assigned_identity.this.client_id
   }
 
+  tags = var.tags
+
+  depends_on = [
+    azurerm_key_vault_access_policy.this
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "this" {
+  key_vault_id = var.key_vault_id
+  tenant_id    = var.tenant_id
+  object_id    = azurerm_user_assigned_identity.this.principal_id
+
+  key_permissions = [
+    "Get", "WrapKey", "UnwrapKey"
+  ]
 }
 
 resource "azurerm_user_assigned_identity" "this" {
   resource_group_name = var.azurerm_resource_group
   location            = data.azurerm_resource_group.this.location
 
-  name = "registry-uai"
+  name = var.name
 }
 
 data "azurerm_key_vault_key" "this" {
-  name         = var.azurerm_key_vault_key_name
+  name         = azurerm_key_vault_key.generated.name
   key_vault_id = var.key_vault_id
+}
+
+resource "azurerm_key_vault_key" "generated" {
+  name         = var.name
+  key_vault_id = var.key_vault_id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
 }
--- terraform-modules/azure/container_registry/variables.tf
+++ terraform-modules/azure/container_registry/variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-    description = "The name of the container registry and used for name prefixes"
+    description = "The name of the container registry and used for name prefixes. alpha numeric characters only are allowed"
     default     = "container_registry"
 }
 
@@ -19,10 +19,18 @@ variable "sku" {
 #     default     = ["xxxxx"]
 # }
 
-variable "azurerm_key_vault_key_name" {
-  description = "Specifies the name of the Key Vault Key."
-}
+# variable "azurerm_key_vault_key_name" {
+#   description = "Specifies the name of the Key Vault Key."
+# }
 
 variable "key_vault_id" {
   description = "(Required) The ID of the Key Vault where the Key should be created. Changing this forces a new resource to be created."
+}
+
+variable "tenant_id" {
+  
+}
+
+variable "tags" {
+  default = {}
 }
