8183afde78575dff55765b210365db60723045c4
Bayron Carranza <bayron.carranza@gmail.com>
2023-02-10 15:21:42 -0600

    EKS 1.23 cis driver compatibility (#403)

--- terraform-modules/aws/eks/main.tf
+++ terraform-modules/aws/eks/main.tf
@@ -31,11 +31,54 @@ resource "aws_kms_key" "eks" {
   tags        = var.tags
 }
 
-
 module "kms_cloudwatch_log_group" {
   source                  = "github.com/ManagedKube/kubernetes-ops.git//terraform-modules/aws/kms/cloudwatch_log_group?ref=v2.0.37"
   log_group_name          = "/aws/eks/${var.cluster_name}/cluster"
   tags                    = var.tags
+}
+
+//EKS 1.23
+/* It needs Amazon EBS CSI driver in order to mount volume. 
+https://docs.aws.amazon.com/eks/latest/userguide/kubernetes-versions.html
+https://docs.aws.amazon.com/eks/latest/userguide/ebs-csi.html
+ebs csi driver is available since 1.14 so it shouldn't impact in previous versions
+that it's using this module.
+https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
+*/
+
+resource "aws_eks_addon" "csi_driver" {
+  cluster_name             = module.eks.cluster_id
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.11.4-eksbuild.1"
+  service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
+}
+
+data "aws_iam_policy_document" "csi" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks.oidc_provider, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+
+    principals {
+      identifiers = [module.eks.oidc_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_ebs_csi_driver" {
+  assume_role_policy = data.aws_iam_policy_document.csi.json
+  name               = "eks-ebs-csi-driver"
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
+  role       = aws_iam_role.eks_ebs_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
 
