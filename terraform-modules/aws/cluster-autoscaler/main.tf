c903e55817ce20d3b35120e29736fd1eb138a783
olamide005 <olamideoladoyin005@gmail.com>
2023-03-06 15:53:09 -0600

    Adding New IAM Policy for Cluster Autoscaler

--- terraform-modules/aws/cluster-autoscaler/main.tf
+++ terraform-modules/aws/cluster-autoscaler/main.tf
@@ -23,7 +23,9 @@ data "aws_iam_policy_document" "cluster_autoscaler" {
       "autoscaling:DescribeAutoScalingGroups",
       "autoscaling:DescribeAutoScalingInstances",
       "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeScalingActivities",
       "autoscaling:DescribeTags",
+      "ec2:DescribeInstanceTypes",
       "ec2:DescribeLaunchTemplateVersions",
     ]
 
@@ -38,6 +40,9 @@ data "aws_iam_policy_document" "cluster_autoscaler" {
       "autoscaling:SetDesiredCapacity",
       "autoscaling:TerminateInstanceInAutoScalingGroup",
       "autoscaling:UpdateAutoScalingGroup",
+      "ec2:DescribeImages",
+      "ec2:GetInstanceTypesFromInstanceRequirements",
+      "eks:DescribeNodegroup"
     ]
 
     resources = ["*"]
