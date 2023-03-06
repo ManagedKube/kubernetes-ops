7b8fddc3120437d7acb8d6da4e1eaf8470cd993b
Prasanth Koya <prasanth.koya@sage.com>
2023-03-06 13:09:17 -0600

    [IAOPSPS-446]-updating the tag key

--- terraform-modules/aws/cloudwan-vpn-connection/main.tf
+++ terraform-modules/aws/cloudwan-vpn-connection/main.tf
@@ -1,9 +1,14 @@
+# locals {
+#   customer_gateway_id = toset (var.customer_gateway_id)
+# }
+
 resource "aws_vpn_connection" "main" {
+  #for_each = local.customer_gateway_id
+  #customer_gateway_id =each.key
   count = var.create_VPN_connection ? length(var.customer_gateway_id) : 0
   customer_gateway_id        = element(var.customer_gateway_id, count.index)
   type                = "ipsec.1"
   static_routes_only  = var.static_routes_only
-
   tags = var.tags
 }
 
@@ -13,6 +18,6 @@ resource "aws_networkmanager_site_to_site_vpn_attachment" "test" {
   core_network_id    = var.core_network_id
   vpn_connection_arn = resource.aws_vpn_connection.main[count.index].arn
   tags = {
-    segment = var.segment_name
+    env = var.segment_name
   }
 }
