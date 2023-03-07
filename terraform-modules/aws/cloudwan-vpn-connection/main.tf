854e77015ebe8e9c6e6d9b91561873628583dd6c
Prasanth Koya <prasanth.koya@sage.com>
2023-03-07 09:45:15 -0600

    [IAOPSPS-446]-updating the VPN module loop

--- terraform-modules/aws/cloudwan-vpn-connection/main.tf
+++ terraform-modules/aws/cloudwan-vpn-connection/main.tf
@@ -1,12 +1,11 @@
-# locals {
-#   customer_gateway_id = toset (var.customer_gateway_id)
-# }
+locals {
+  customer_gateway_id = toset (var.customer_gateway_id)
+  vpn_connection_arn = toset ([for my_arn in aws_vpn_connection.main: my_arn.arn])
+}
 
 resource "aws_vpn_connection" "main" {
-  #for_each = local.customer_gateway_id
-  #customer_gateway_id =each.key
-  count = var.create_VPN_connection ? length(var.customer_gateway_id) : 0
-  customer_gateway_id        = element(var.customer_gateway_id, count.index)
+  for_each = local.customer_gateway_id
+  customer_gateway_id =each.key
   type                = "ipsec.1"
   static_routes_only  = var.static_routes_only
   tags = var.tags
@@ -14,9 +13,9 @@ resource "aws_vpn_connection" "main" {
 
 
 resource "aws_networkmanager_site_to_site_vpn_attachment" "test" {
-  count = var.create_VPN_connection ? length(var.customer_gateway_id) : 0
+  for_each = local.vpn_connection_arn
+  vpn_connection_arn = each.key
   core_network_id    = var.core_network_id
-  vpn_connection_arn = resource.aws_vpn_connection.main[count.index].arn
   tags = {
     env = var.segment_name
   }
