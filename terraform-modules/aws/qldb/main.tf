fdd897d38f9a0a1b82e68dde454593ee0da3dc61
sakruthiJupalli <sjupalli@exactpay.com>
2023-03-15 18:27:38 -0400

    added vpc endpoint for qldb

--- terraform-modules/aws/qldb/main.tf
+++ terraform-modules/aws/qldb/main.tf
@@ -5,7 +5,7 @@ resource "aws_qldb_ledger" "this" {
   tags                = var.tags
 }
 
-resource "aws_security_group" "this" {
+resource "aws_security_group" "qldb" {
   name        = "qldb-${var.name}"
   description = "qldb security group"
   vpc_id      = var.vpc_id
@@ -25,6 +25,19 @@ resource "aws_security_group" "this" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
+  tags = var.tags
+}
+
+resource "aws_vpc_endpoint" "qldb" {
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.us-west-2.qldb.session"
+  vpc_endpoint_type   = "Interface"
+
+  security_group_ids  = [aws_security_group.qldb.id]
+  subnet_ids          = var.subnet_ids
+
+  private_dns_enabled = true
 
   tags = var.tags
 }
