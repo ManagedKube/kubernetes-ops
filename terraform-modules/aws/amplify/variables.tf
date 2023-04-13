58d7fd0f0a6aabec243e390b2cc31551c4c27279
sakruthiJupalli <sjupalli@exactpay.com>
2023-04-13 19:38:24 -0400

    add subdomain block

--- terraform-modules/aws/amplify/variables.tf
+++ terraform-modules/aws/amplify/variables.tf
@@ -52,3 +52,13 @@ variable "domain_name" {
   description = "The domain name to associate with the Amplify app."
   type        = string
 }
+
+variable "sub_domain_prefix" {
+  description = "The subdomain prefix to associate with the branch."
+  type        = string
+}
+
+variable "sub_domain_branch" {
+  description = "The branch name to associate with the subdomain."
+  type        = string
+}
