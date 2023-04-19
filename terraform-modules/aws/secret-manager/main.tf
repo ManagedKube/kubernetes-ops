f11a0505bba924cdce9db51b8b20789b4001cfbc
Bayron Carranza <bayron.carranza@gmail.com>
2023-04-19 16:43:18 -0600

    secret manager fix

--- terraform-modules/aws/secret-manager/main.tf
+++ terraform-modules/aws/secret-manager/main.tf
@@ -1,6 +1,6 @@
 # Creates the KMS key only when the create_kms_key variable is set to false.
 resource "aws_kms_key" "this" {
-  count                   = var.create_kms_key ? 0 : 1
+  count                   = var.create_kms_key ? 1 : 0
   description             = var.secretsmanager_kms_name
   deletion_window_in_days = var.secretsmanager_kms_deletion_window_in_days
   tags                    = var.tags
@@ -13,7 +13,7 @@ resource "aws_secretsmanager_secret" "this" {
   recovery_window_in_days   = var.secretsmanager_secret_recovery_window_in_days
   
   #If you don't specify this value, then Secrets Manager defaults to using the AWS account's default KMS key (the one named aws/secretsmanager
-  kms_key_id                = var.create_kms_key ? null : aws_kms_key.this[0].id
+  kms_key_id                = var.create_kms_key ? aws_kms_key.this[0].id : null
   
   tags                      = var.tags
 }
