bee9bfca67ff889ba0b7fee2656a603cc186f070
Bayron Carranza <bayron.carranza@gmail.com>
2023-04-20 11:45:35 -0600

    fix msk

--- terraform-modules/aws/msk/outputs.tf
+++ terraform-modules/aws/msk/outputs.tf
@@ -20,50 +20,50 @@ output "bootstrap_brokers" {
 
 output "bootstrap_brokers_tls" {
   description = "A comma separated list of one or more DNS names (or IPs) and TLS port pairs kafka brokers suitable to boostrap connectivity to the kafka cluster"
-  value       = module.bootstrap_brokers_tls
+  value       = module.msk.bootstrap_brokers_tls
 }
 
 output "bootstrap_brokers_scram" {
   description = "A comma separated list of one or more DNS names (or IPs) and TLS port pairs kafka brokers suitable to boostrap connectivity using SASL/SCRAM to the kafka cluster."
-  value       = module.bootstrap_brokers_scram
+  value       = module.msk.bootstrap_brokers_scram
 }
 
 output "bootstrap_brokers_iam" {
   description = "A comma separated list of one or more DNS names (or IPs) and TLS port pairs kafka brokers suitable to boostrap connectivity using SASL/IAM to the kafka cluster."
-  value       = module.bootstrap_brokers_iam
+  value       = module.msk.bootstrap_brokers_iam
 }
 
 output "all_brokers" {
   description = "A list of all brokers"
-  value       = module.all_brokers
+  value       = module.msk.all_brokers
 }
 
 output "current_version" {
   description = "Current version of the MSK Cluster used for updates"
-  value       = module.current_version
+  value       = module.msk.current_version
 }
 
 output "config_arn" {
   description = "Amazon Resource Name (ARN) of the configuration"
-  value       = module.config_arn
+  value       = module.msk.config_arn
 }
 
 output "latest_revision" {
   description = "Latest revision of the configuration"
-  value       = module.latest_revision
+  value       = module.msk.latest_revision
 }
 
 output "hostname" {
   description = "Comma separated list of one or more MSK Cluster Broker DNS hostname"
-  value       = module.hostname
+  value       = module.msk.hostname
 }
 
 output "security_group_id" {
   description = "The ID of the security group rule"
-  value       = module.security_group_id
+  value       = module.msk.security_group_id
 }
 
 output "security_group_name" {
   description = "The name of the security group rule"
-  value       = module.security_group_name
+  value       = module.msk.security_group_name
 }
