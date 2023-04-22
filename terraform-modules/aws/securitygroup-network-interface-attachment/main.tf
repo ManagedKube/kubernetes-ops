b9bca4df1cd4e66531750a207d2634183a18ceb7
Bayron Carranza <bayron.carranza@gmail.com>
2023-04-21 22:07:17 -0600

    fix

--- terraform-modules/aws/securitygroup-network-interface-attachment/main.tf
+++ terraform-modules/aws/securitygroup-network-interface-attachment/main.tf
@@ -1,16 +1,19 @@
-data "aws_instances" "ec2" {
+data "aws_instances" "ec2list" {
   filter {
     name   = "tag:${var.fetch_ec2_instance_tag_name}"
-    values = [var.fetch_ec2_instance_name] 
+    values = [var.fetch_ec2_instance_name]
   }
 }
 
-output "instance_ids" {
-  value = tolist(data.aws_instances.ec2.ids)
+locals {
+  primary_network_interface_ids = {
+    for id in data.aws_instances.ec2list.ids :
+    id => data.aws_instances.ec2list.instances[id].primary_network_interface_id
+  }
 }
 
 resource "aws_network_interface_sg_attachment" "extra_sg_association" {
-  count                     = length(data.aws_instances.ec2.ids)
-  network_interface_id      = data.aws_instances.ec2.ids[count.index].primary_network_interface_id
-  security_group_id         = var.fetch_ec2_instance_sg_id
+  for_each             = local.primary_network_interface_ids
+  network_interface_id = each.value
+  security_group_id    = var.fetch_ec2_instance_sg_id
 }
