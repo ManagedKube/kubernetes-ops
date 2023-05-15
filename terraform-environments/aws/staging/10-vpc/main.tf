71730329beb63f2f73670794a2636ec807afc629
davidabouhalaka <davidabouhalaka@gmail.com>
2023-05-15 14:06:35 -0500

    fix typo

--- terraform-environments/aws/staging/10-vpc/main.tf
+++ terraform-environments/aws/staging/10-vpc/main.tf
@@ -41,7 +41,7 @@ module "vpc" {
   aws_region       = local.aws_region
   azs              = ["us-east-1a", "us-east-1c"]
   vpc_cidr         = "10.0.0.0/16"
-  secodary_cidrs   = []
+  secondary_cidrs   = []
   k8s_worker_subnets = []
   private_subnets  = []
   public_subnets   = ["10.0.101.0/24", "10.0.102.0/24"]
