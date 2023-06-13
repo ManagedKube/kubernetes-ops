635499ca16a99e226647a0448c066d92fcb28325
bcarranza <bayron.carranza@gmail.com>
2023-06-13 11:26:32 -0600

    add http

--- terraform-modules/aws/nlb/main.tf
+++ terraform-modules/aws/nlb/main.tf
@@ -46,7 +46,7 @@ resource "aws_lb_target_group" "default" {
     enabled             = var.health_check_enabled
     port                = local.health_check_port
     protocol            = local.health_check_protocol
-    path                = "HTTP"
+    path                = local.health_check_protocol == "HTTP" ? var.health_check_path : null
     healthy_threshold   = var.health_check_threshold
     unhealthy_threshold = local.unhealthy_threshold
     interval            = var.health_check_interval
