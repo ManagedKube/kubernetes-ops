9da2e9914f87817d7de0f6ec45b3dcb26511c692
bcarranza <bayron.carranza@gmail.com>
2023-06-13 15:43:41 -0600

    vpc link

--- terraform-modules/aws/api-gateway/vpc-link/main.tf
+++ terraform-modules/aws/api-gateway/vpc-link/main.tf
@@ -1,15 +1,6 @@
-resource "aws_lb" "apivpclinknlb" {
-  name               = var.load_balancer_name
-  internal           = var.internal_lb
-  load_balancer_type = var.load_balancer_type
-
-  subnet_mapping {
-    subnet_id = var.subnet_id
-  }
-}
-
 resource "aws_api_gateway_vpc_link" "apivpclink" {
   name        = var.vpc_link_name
   description = var.vpc_link_description
-  target_arns = [aws_lb.apivpclinknlb.arn]
+  target_arns = [var.vpc_link_nbl_arn]
+  tags = var.tags
 }
--- terraform-modules/aws/api-gateway/vpc-link/variables.tf
+++ terraform-modules/aws/api-gateway/vpc-link/variables.tf
@@ -1,7 +1,3 @@
-variable "subnet_id" {
-  description = "ID of the subnet for the load balancer"
-}
-
 variable "vpc_link_name" {
   description = "Name of the API Gateway VPC link"
 }
@@ -10,22 +6,8 @@ variable "vpc_link_description" {
   description = "Description of the API Gateway VPC link"
 }
 
-# Load balancer
-variable "subnet_id" {
-  description = "ID of the subnet for the load balancer"
-}
-
-variable "load_balancer_name" {
-  description = "Name of the load balancer"
-}
-
-variable "internal_lb" {
-  description = "Flag to indicate if the load balancer is internal (true/false)"
-  type        = bool
-}
-
-variable "load_balancer_type" {
-  description = "Type of load balancer"
+variable "vpc_link_nbl_arn" {
+  description = "ARN of the NLB VPC link"
 }
 
 variable "tags" {
