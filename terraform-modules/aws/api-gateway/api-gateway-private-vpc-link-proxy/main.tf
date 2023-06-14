6eff487c7729e032b28c8ccac6f9a5337eaa923c
bcarranza <bayron.carranza@gmail.com>
2023-06-14 08:45:08 -0600

    Proxy

--- terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
+++ terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
@@ -26,9 +26,9 @@ resource "aws_api_gateway_integration" "vpc_proxy" {
   rest_api_id = aws_api_gateway_rest_api.my_api.id
   resource_id = aws_api_gateway_resource.proxy_resource.id
   http_method = aws_api_gateway_method.proxy_method.http_method
-  type = "VPC_PROXY"
-  vpc_link_id = var.vpc_link_id
-  endpoint_url = var.api_gateway_b_uri
+  connection_type = "VPC_LINK"
+  connection_id = var.vpc_link_id
+  uri = var.api_gateway_b_uri
   integration_http_method = "ANY"
   passthrough_behavior = "WHEN_NO_MATCH"
   request_parameters = {}
@@ -39,7 +39,7 @@ resource "aws_api_gateway_method_response" "proxy" {
   resource_id = aws_api_gateway_resource.proxy_resource.id
   http_method = aws_api_gateway_method.proxy_method.http_method
   status_code = "200"
-  response_templates = {
+  response_parameteresponse_parameters = {
     "application/json" = ""
   }
 }
