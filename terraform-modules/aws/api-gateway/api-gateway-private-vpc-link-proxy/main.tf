d4f2032864cae843fa8acaebbe88e673ebd9d87e
bcarranza <bayron.carranza@gmail.com>
2023-06-14 08:38:15 -0600

    integration + response

--- terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
+++ terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
@@ -22,19 +22,41 @@ resource "aws_api_gateway_method" "proxy_method" {
   }
 }
 
-resource "aws_api_gateway_integration" "proxy_integration" {
-  rest_api_id          = aws_api_gateway_rest_api.my_api.id
-  resource_id          = aws_api_gateway_resource.proxy_resource.id
-  http_method          = aws_api_gateway_method.proxy_method.http_method
-  type                 = "VPC_PROXY"
-  uri                  = var.api_gateway_b_uri
+resource "aws_api_gateway_integration" "vpc_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.proxy_resource.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+  type = "VPC_PROXY"
+  vpc_link_id = var.vpc_link_id
+  endpoint_url = var.api_gateway_b_uri
   integration_http_method = "ANY"
+  passthrough_behavior = "WHEN_NO_MATCH"
+  request_parameters = {}
+}
 
-  cache_key_parameters = ["method.request.path.proxy"]
-  request_parameters = {
-    "integration.request.path.proxy" = "method.request.path.proxy"
+resource "aws_api_gateway_method_response" "proxy" {
+  rest_api_id = aws_api_gateway_rest_api.my_api.id
+  resource_id = aws_api_gateway_resource.proxy_resource.id
+  http_method = aws_api_gateway_method.proxy_method.http_method
+  status_code = "200"
+  response_templates = {
+    "application/json" = ""
   }
 }
+
+#resource "aws_api_gateway_integration" "proxy_integration" {
+#  rest_api_id          = aws_api_gateway_rest_api.my_api.id
+#  resource_id          = aws_api_gateway_resource.proxy_resource.id
+#  http_method          = aws_api_gateway_method.proxy_method.http_method
+#  type                 = "VPC_PROXY"
+#  uri                  = var.api_gateway_b_uri
+#  integration_http_method = "ANY"
+
+#  cache_key_parameters = ["method.request.path.proxy"]
+#  request_parameters = {
+#    "integration.request.path.proxy" = "method.request.path.proxy"
+#  }
+#}
 
 #resource "aws_api_gateway_integration_response" "proxy_integration_response" {
 #  rest_api_id     = aws_api_gateway_rest_api.my_api.id
