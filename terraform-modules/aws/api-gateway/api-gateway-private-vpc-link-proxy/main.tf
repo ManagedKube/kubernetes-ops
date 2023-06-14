b781e158923947e4e5e7640d9e9d291463f8f665
bcarranza <bayron.carranza@gmail.com>
2023-06-14 10:55:59 -0600

    api gateway integration

--- terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
+++ terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
@@ -34,11 +34,11 @@ resource "aws_api_gateway_integration" "vpc_proxy" {
   type = "HTTP_PROXY"
 }
 
-resource "aws_api_gateway_method_response" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.my_api.id
-  resource_id = aws_api_gateway_resource.proxy_resource.id
-  http_method = aws_api_gateway_method.proxy_method.http_method
-}
+#resource "aws_api_gateway_method_response" "proxy" {
+#  rest_api_id = aws_api_gateway_rest_api.my_api.id
+#  resource_id = aws_api_gateway_resource.proxy_resource.id
+#  http_method = aws_api_gateway_method.proxy_method.http_method
+#}
 
 #resource "aws_api_gateway_integration" "proxy_integration" {
 #  rest_api_id          = aws_api_gateway_rest_api.my_api.id
