078a3afce0235cd55a826f2b8190d067392a15d9
bcarranza <bayron.carranza@gmail.com>
2023-06-14 11:18:07 -0600

    response_parameters

--- terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
+++ terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
@@ -49,10 +49,12 @@ resource "aws_api_gateway_method_response" "options_200" {
     resource_id   = "${aws_api_gateway_resource.proxy_resource.id}"
     http_method   = "${aws_api_gateway_method.options_method.http_method}"
     status_code   = "200"
-    response_models {
-      "application/json" = "Empty"
+    
+    response_models = {
+        "application/json" = "Empty"
     }
-    response_parameters {
+
+    response_parameters = {
         "method.response.header.Access-Control-Allow-Headers" = true,
         "method.response.header.Access-Control-Allow-Methods" = true,
         "method.response.header.Access-Control-Allow-Origin" = true
