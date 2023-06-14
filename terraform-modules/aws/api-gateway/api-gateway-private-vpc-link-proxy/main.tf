c482eeace1b168fa5bbc4d0fdb58a9e87b062b16
bcarranza <bayron.carranza@gmail.com>
2023-06-14 08:06:02 -0600

    proxy_integration

--- terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
+++ terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
@@ -22,7 +22,7 @@ resource "aws_api_gateway_method" "proxy_method" {
   }
 }
 
-resource "aws_api_gateway_integration" "MyDemoIntegration" {
+resource "aws_api_gateway_integration" "proxy_integration" {
   rest_api_id          = aws_api_gateway_rest_api.my_api.id
   resource_id          = aws_api_gateway_resource.proxy_resource.id
   http_method          = aws_api_gateway_method.proxy_method.http_method
