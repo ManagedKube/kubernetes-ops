b74fc10a243a1b55429470d15dc3d0497e6a0083
bcarranza <bayron.carranza@gmail.com>
2023-06-14 12:06:35 -0600

    IntegrationResponse

--- terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
+++ terraform-modules/aws/api-gateway/api-gateway-private-vpc-link-proxy/main.tf
@@ -46,7 +46,7 @@ resource "aws_api_gateway_method_response" "m200" {
     }
 }
 
-resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+resource "aws_api_gateway_integration_response" "IntegrationResponse" {
    rest_api_id = "${aws_api_gateway_rest_api.my_api.id}"
    resource_id = "${aws_api_gateway_resource.proxy_resource.id}"
    http_method = "${aws_api_gateway_method.proxy_method.http_method}"
