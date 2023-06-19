282157daeebf379033378764d382a6112d618fd1
Bayron Carranza <bayron.carranza@gmail.com>
2023-06-19 15:26:23 -0600

    Update main.tf

--- terraform-modules/aws/ec2-tag/main.tf
+++ terraform-modules/aws/ec2-tag/main.tf
@@ -4,9 +4,18 @@ data "aws_instances" "existing_instances" {
 
 data "aws_caller_identity" "current" {}
 
-resource "aws_ec2_tag" "tag_existing_instances" {
-  for_each    = toset(data.aws_instances.existing_instances.ids)
-  resource_id = each.key
-
+locals {
+  tags = {
+    for instance_id, instance_tags in data.aws_instances.existing_instances.ids : instance_id => {
+      for variable in ["Env", "Platform", "Domain_2", "Domain_3"] : variable => var.account_tags[data.aws_caller_identity.current.account_id][variable]
+      if var.account_tags[data.aws_caller_identity.current.account_id][variable] != null
+    }
+  }
 }
 
+resource "aws_instance_tag" "tag_existing_instances" {
+  count       = length(data.aws_instances.existing_instances.ids)
+  instance_id = data.aws_instances.existing_instances.ids[count.index]
+
+  tags = local.tags[data.aws_instances.existing_instances.ids[count.index]]
+}
