baa34acce2ece9d32e3f70b4e66b5781875f4aca
Bayron Carranza <bayron.carranza@gmail.com>
2023-06-19 15:16:18 -0600

    debug mode

--- terraform-modules/aws/ec2-tag/main.tf
+++ terraform-modules/aws/ec2-tag/main.tf
@@ -8,18 +8,5 @@ resource "aws_ec2_tag" "tag_existing_instances" {
   for_each    = toset(data.aws_instances.existing_instances.ids)
   resource_id = each.key
 
-  dynamic "tag" {
-    for_each = [
-      for tag_key, tag_value in var.account_tags[data.aws_caller_identity.current.account_id] : {
-        key   = tag_key
-        value = tag_value
-      }
-      if tag_key in ["Env", "Platform", "Domain_2", "Domain_3"]
-    ]
-
-    content {
-      key   = tag.key
-      value = tag.value
-    }
-  }
 }
+
