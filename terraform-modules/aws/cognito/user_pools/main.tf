83a23684be969db9769d05f365adcb5326d167da
garland <garland.kan@anthem.com>
2023-06-22 16:11:40 -0700

    Adding identity provider

--- terraform-modules/aws/cognito/user_pools/main.tf
+++ terraform-modules/aws/cognito/user_pools/main.tf
@@ -10,3 +10,19 @@ resource "aws_cognito_user_pool_domain" "main" {
 }
 
 data "aws_region" "current" {}
+
+resource "aws_cognito_identity_provider" "main" {
+  user_pool_id  = aws_cognito_user_pool.pool.id
+  provider_name = "Okta"
+  provider_type = "SAML"
+
+  provider_details = {
+    authorize_scopes = "email"
+    client_id        = var.client_id
+    client_secret    = var.client_secret
+
+  attribute_mapping = {
+    email    = "email"
+    username = "sub"
+  }
+}
--- terraform-modules/aws/cognito/user_pools/variables.tf
+++ terraform-modules/aws/cognito/user_pools/variables.tf
@@ -5,3 +5,11 @@ variable "tags" {
 variable "name" {
   description = "The name of the user pool.  This should be unique across your AWS account.  Putting in an env name in here is recommended."
 }
+
+variable "client_id" {
+  description = "The client id of the Okta app."
+}
+  
+variable "client_secret" {
+  description = "The client secret of the Okta app."
+}
