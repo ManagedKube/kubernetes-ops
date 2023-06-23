a4da0d5c2119c6918f0de45430be3370892ae336
garland <garland.kan@anthem.com>
2023-06-23 06:19:36 -0700

    Adding login endpoint url

--- terraform-modules/aws/cognito/user_pools/outputs.tf
+++ terraform-modules/aws/cognito/user_pools/outputs.tf
@@ -14,3 +14,8 @@ output "endpoint" {
 output "cognito_domain" {
   value = "https://${var.name}.auth.${data.aws_region.current.name}.amazoncognito.com"
 }
+
+## Section: Construct the endpoint URL
+output "login_endpoint" {
+  value = "https://${var.name}.auth.${data.aws_region.current.name}.amazoncognito.com/login?response_type=token&client_id=${aws_cognito_user_pool_client.main.id}&redirect_uri=${var.aws_cognito_user_pool_client_callback_urls[0]}"
+}
