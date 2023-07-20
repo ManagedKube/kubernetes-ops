c8f71be15964c56127299e5a9124f486054209ee
ana-ghirghilijiu-sage <ana.ghirghilijiu@sage.com>
2023-07-20 16:55:25 -0500

    [IAOPSPS-1503] adding route53_vpc_association

--- terraform-modules/aws/route53/vpc-association/main.tf
+++ terraform-modules/aws/route53/vpc-association/main.tf
@@ -3,17 +3,19 @@
 #   vpc_id   = each.value
 #   zone_id  = var.route53_hosted_zone_id
 # }
+
 resource "aws_route53_zone_association" "this" {
-  for_each = var.vpc_id
-  vpc_id   = each.value.id
-  vpc_region = each.value.region
-  zone_id = var.route53_hosted_zone_id
+  for_each = var.hosted_zone
+
+  zone_id  = lookup(each.value, "zone_id", each.key)
+  vpc_id   = lookup(each.value, "vpc_id", null)
 
   dynamic "vpc" {
-    for_each = try([each.value], [])
+    for_each = try(tolist(lookup(each.value, "vpc", [])), [lookup(each.value, "vpc", {})])
+
     content {
-      vpc_id     = vpc.value.id
-      vpc_region = vpc.value.region
+      vpc_id     = vpc.value.vpc_id
+      vpc_region = lookup(vpc.value, "vpc_region", null)
     }
   }
 }
