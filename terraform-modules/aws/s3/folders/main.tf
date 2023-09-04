94ccf88e1333ae68792d04f992663dcb2d143d92
bcarranza <bayron.carranza@gmail.com>
2023-09-04 16:53:55 -0600

    fix

--- terraform-modules/aws/s3/folders/main.tf
+++ terraform-modules/aws/s3/folders/main.tf
@@ -5,7 +5,7 @@ locals {
 resource "aws_s3_object" "directory_structure" {
   for_each = local.folder_structure_map
 
-  bucket = aws_s3_bucket.example.id
+  bucket = var.bucket_name
   key    = each.value
   content_type = "application/x-directory"
 }
