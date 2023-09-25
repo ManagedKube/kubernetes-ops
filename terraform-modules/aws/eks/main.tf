3123b9a9cb0b4ad2f73de735fc46685e26ec7684
bcarranza <bayron.carranza@gmail.com>
2023-09-25 15:00:39 -0600

    19.15.4

--- terraform-modules/aws/eks/main.tf
+++ terraform-modules/aws/eks/main.tf
@@ -84,7 +84,7 @@ resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
 
 module "eks" {
   source           = "terraform-aws-modules/eks/aws"
-  version          = var.eks_terraform_version #"18.23.0"
+  version          = "19.15.4"
   cluster_name     = var.cluster_name
   cluster_version  = var.cluster_version
   enable_irsa      = var.enable_irsa
