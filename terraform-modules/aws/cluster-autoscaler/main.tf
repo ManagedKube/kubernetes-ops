ec610c2fb213fa90685ae63457da224aa744650a
bcarranza <bayron.carranza@gmail.com>
2023-12-27 21:21:29 -0600

    testing

--- terraform-modules/aws/cluster-autoscaler/main.tf
+++ terraform-modules/aws/cluster-autoscaler/main.tf
@@ -1,62 +1,62 @@
-#module "iam_assumable_role_admin" {
-#  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-#  version                       = "5.33.0"
-#  create_role                   = true
-#  role_name                     = "cluster-autoscaler-${var.cluster_name}"
-#  provider_url                  = replace(var.eks_cluster_oidc_issuer_url, "https://", "")
-#  role_policy_arns              = [aws_iam_policy.cluster_autoscaler.arn]
-#  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.k8s_service_account_namespace}:${var.k8s_service_account_name}"]
-#}
+# module "iam_assumable_role_admin" {
+#   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+#   version                       = "5.33.0"
+#   create_role                   = true
+#   role_name                     = "cluster-autoscaler-${var.cluster_name}"
+#   provider_url                  = replace(var.eks_cluster_oidc_issuer_url, "https://", "")
+#   role_policy_arns              = [aws_iam_policy.cluster_autoscaler.arn]
+#   oidc_fully_qualified_subjects = ["system:serviceaccount:${var.k8s_service_account_namespace}:${var.k8s_service_account_name}"]
+# }
 
-resource "aws_iam_policy" "cluster_autoscaler" {
-  name_prefix = "cluster-autoscaler-${var.cluster_name}"
-  description = "EKS cluster-autoscaler policy for cluster ${var.eks_cluster_id}"
-  policy      = data.aws_iam_policy_document.cluster_autoscaler.json
-}
+# resource "aws_iam_policy" "cluster_autoscaler" {
+#   name_prefix = "cluster-autoscaler-${var.cluster_name}"
+#   description = "EKS cluster-autoscaler policy for cluster ${var.eks_cluster_id}"
+#   policy      = data.aws_iam_policy_document.cluster_autoscaler.json
+# }
 
-data "aws_iam_policy_document" "cluster_autoscaler" {
-  statement {
-    sid    = "clusterAutoscalerAll"
-    effect = "Allow"
+# data "aws_iam_policy_document" "cluster_autoscaler" {
+#   statement {
+#     sid    = "clusterAutoscalerAll"
+#     effect = "Allow"
 
-    actions = [
-      "autoscaling:DescribeAutoScalingGroups",
-      "autoscaling:DescribeAutoScalingInstances",
-      "autoscaling:DescribeLaunchConfigurations",
-      "autoscaling:DescribeTags",
-      "ec2:DescribeLaunchTemplateVersions",
-    ]
+#     actions = [
+#       "autoscaling:DescribeAutoScalingGroups",
+#       "autoscaling:DescribeAutoScalingInstances",
+#       "autoscaling:DescribeLaunchConfigurations",
+#       "autoscaling:DescribeTags",
+#       "ec2:DescribeLaunchTemplateVersions",
+#     ]
 
-    resources = ["*"]
-  }
+#     resources = ["*"]
+#   }
 
-  statement {
-    sid    = "clusterAutoscalerOwn"
-    effect = "Allow"
+#   statement {
+#     sid    = "clusterAutoscalerOwn"
+#     effect = "Allow"
 
-    actions = [
-      "autoscaling:SetDesiredCapacity",
-      "autoscaling:TerminateInstanceInAutoScalingGroup",
-      "autoscaling:UpdateAutoScalingGroup",
-    ]
+#     actions = [
+#       "autoscaling:SetDesiredCapacity",
+#       "autoscaling:TerminateInstanceInAutoScalingGroup",
+#       "autoscaling:UpdateAutoScalingGroup",
+#     ]
 
-    resources = ["*"]
+#     resources = ["*"]
 
-    condition {
-      test     = "StringEquals"
-      variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${var.eks_cluster_id}"
-      values   = ["owned"]
-    }
+#     condition {
+#       test     = "StringEquals"
+#       variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${var.eks_cluster_id}"
+#       values   = ["owned"]
+#     }
 
-    condition {
-      test     = "StringEquals"
-      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
-      values   = ["true"]
-    }
-  }
-}
+#     condition {
+#       test     = "StringEquals"
+#       variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
+#       values   = ["true"]
+#     }
+#   }
+# }
 
-data "aws_caller_identity" "current" {}
+# data "aws_caller_identity" "current" {}
 
 module "cluster-autoscaler" {
   source = "github.com/ManagedKube/kubernetes-ops//terraform-modules/aws/helm/helm_generic?ref=v1.0.9"
