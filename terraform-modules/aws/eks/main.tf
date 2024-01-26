0c822649fb065f3b5953def87d9daff6ed0c0da3
bcarranza <bayron.carranza@gmail.com>
2024-01-26 12:36:30 -0600

    EKS-MULTI-ADDONS

--- terraform-modules/aws/eks/main.tf
+++ terraform-modules/aws/eks/main.tf
@@ -46,12 +46,6 @@ that it's using this module.
 https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
 */
 
-resource "aws_eks_addon" "csi_driver" {
-  cluster_name             = module.eks.cluster_id
-  addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.11.4-eksbuild.1"
-  service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
-}
 
 data "aws_iam_policy_document" "csi" {
   statement {
@@ -124,4 +118,6 @@ module "eks" {
   aws_auth_users = var.aws_auth_users
 
   aws_auth_accounts = var.aws_auth_accounts
+
+  cluster_addons = var.cluster_addons 
 }
