5e3b7abd66ae71280acc156e4b0f6afd9a6ec3e2
bcarranza <bayron.carranza@gmail.com>
2024-01-26 13:45:01 -0600

    remove iam role for cni cause it will handle by addons

--- terraform-modules/aws/eks/main.tf
+++ terraform-modules/aws/eks/main.tf
@@ -65,17 +65,6 @@ data "aws_iam_policy_document" "csi" {
   }
 }
 
-resource "aws_iam_role" "eks_ebs_csi_driver" {
-  assume_role_policy = data.aws_iam_policy_document.csi.json
-  name               = "eks-ebs-csi-driver"
-}
-
-resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
-  role       = aws_iam_role.eks_ebs_csi_driver.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
-
 module "eks" {
   source           = "terraform-aws-modules/eks/aws"
   version          = "18.23.0"
