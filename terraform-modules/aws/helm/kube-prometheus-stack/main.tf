bafead7692b61dc3e1107b387093035ab84eb824
Bayron Carranza <bayron.carranza@gmail.com>
2024-03-13 16:42:43 -0600

    fix oidc role

--- terraform-modules/aws/helm/kube-prometheus-stack/main.tf
+++ terraform-modules/aws/helm/kube-prometheus-stack/main.tf
@@ -38,7 +38,7 @@ module "iam_assumable_role_grafana" {
   role_name                     = "${local.k8s_service_account_name}-${var.environment_name}"
   provider_url                  = replace(var.eks_cluster_oidc_issuer_url, "https://", "")
   role_policy_arns              = [aws_iam_policy.grafana[0].arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${local.k8s_service_account_name}"]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${var.namespace}:${local.k8s_service_account_name}-${var.environment_name}"]
   tags                          = var.tags
 }
 
