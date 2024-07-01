fbeb983c6cf995145d1ba882a3bcb1b732de2cd3
surendra-yelavarthi <149083189+surendra-yelavarthi@users.noreply.github.com>
2024-07-01 11:24:39 -0500

    Fixing csi driver add-on issue

--- terraform-modules/aws/eks/main.tf
+++ terraform-modules/aws/eks/main.tf
@@ -45,13 +45,14 @@ ebs csi driver is available since 1.14 so it shouldn't impact in previous versio
 that it's using this module.
 https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
 */
-
+/*
 resource "aws_eks_addon" "csi_driver" {
   cluster_name             = module.eks.cluster_id
   addon_name               = "aws-ebs-csi-driver"
   addon_version            = "v1.11.4-eksbuild.1"
   service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
 }
+
 
 data "aws_iam_policy_document" "csi" {
   statement {
@@ -80,7 +81,7 @@ resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
   role       = aws_iam_role.eks_ebs_csi_driver.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
-
+*/
 
 module "eks" {
   source           = "terraform-aws-modules/eks/aws"
